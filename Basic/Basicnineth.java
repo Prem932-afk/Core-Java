52119cb9225c21e17a2190a92c7ef269971d45f8
prem <premshekhawat19@gmail.com>
2024-12-10 14:38:45 +0530

    Basic nineth code of java

---
+++ Basic/Basicnineth.java
@@ -0,0 +1,27 @@
+//34+67+23+12+6 
+import java.util.Scanner;
+class Basicnineth
+{
+    public static void main(String args[])
+    {
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your first number");
+        a=sc.nextInt();
+        System.out.println("Enter your second number");
+        b=sc.nextInt();
+        System.out.println("Enter your third number");
+        c=sc.nextInt();
+        System.out.println("Enter your fourth number");
+        d=sc.nextInt();
+        System.out.println("Enter your fivth number");
+        e=sc.nextInt();
+        float f=a*b+c/d-e;
+        System.out.println(f);
+    }
+}
+//56*34+12/2-13 
