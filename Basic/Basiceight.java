6feb6c93636e5d8a65cf688d9c3fe99f6ecb0ee5
prem <premshekhawat19@gmail.com>
2024-12-10 14:33:49 +0530

    Basic eight code of java

---
+++ Basic/Basiceight.java
@@ -0,0 +1,26 @@
+//34+67+23+12+6 
+import java.util.Scanner;
+class Basiceight
+{
+    public static void main(String args[])
+    {
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your first number");
+        a=sc.nextInt();
+        System.out.println("Enter your second number");
+        b=sc.nextInt();
+        System.out.println("Enter your third number");
+        c=sc.nextInt();
+        System.out.println("Enter your fourth number");
+        d=sc.nextInt();
+        System.out.println("Enter your fivth number");
+        e=sc.nextInt();
+        int f=a+b+c+d+e;
+        System.out.println(f);
+    }
+}
