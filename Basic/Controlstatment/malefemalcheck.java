6523286e46fe745af89728470e1463759a39f69d
prem <premshekhawat19@gmail.com>
2024-12-12 15:17:34 +0530

    Check male female

---
+++ Basic/Controlstatment/malefemalcheck.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class malefemalcheck 
+{
+    public static void main(String[] args) {
+    char ch;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your charector");
+    ch=sc.next().charAt(0);
+    if(ch=='m')
+    {
+        System.out.println(" Male");
+    }
+    else
+    {
+        System.out.println("Female");
+    }
+
+    }
+    
+}
