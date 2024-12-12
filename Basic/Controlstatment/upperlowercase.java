df3f40af33e4c69fcf5c39aa9b5d020994e2d0ff
prem <premshekhawat19@gmail.com>
2024-12-12 14:47:06 +0530

    Upper and Lower case

---
+++ Basic/Controlstatment/upperlowercase.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+class upperlowercase{
+    public static void main(String[] args) {
+        char ch;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your charector");
+        ch=sc.next().charAt(0);
+
+        if(ch>='A'&& ch<='Z' )
+        {
+            System.out.println("Charector is Uper case");
+        }
+        else
+        {
+            System.out.println("Charector is lower case");
+        }
+    }
+}
