7d3c8d27294e70884d751b03f8b298ce502148b9
prem <premshekhawat19@gmail.com>
2024-12-12 15:30:22 +0530

    Char match

---
+++ Basic/Controlstatment/charmatch.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+class charmatch 
+{
+    public static void main(String[] args) {
+    char ch;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your charector");
+    ch=sc.next().charAt(0);
+    if(ch=='a')
+    {
+        System.out.println(" Apple");
+    }
+    else if(ch=='b')
+    {
+        System.out.println("Ball");
+    }
+    else if(ch=='c')
+    {
+        System.out.println("cat");
+    }
+    else
+    {
+        System.out.println("Input not match");
+    }
+
+    }
+    
+}
