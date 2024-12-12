cbbff72954f621600d9db3b1be955bf42f41dfde
prem <premshekhawat19@gmail.com>
2024-12-12 15:26:37 +0530

    Evenoddnumber

---
+++ Basic/Controlstatment/evenoddnumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class evenoddnumber
+{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your number");
+        n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Number is even");
+        }
+        else
+        {
+            System.out.println("Number is odd");
+        }
+    }
+}
