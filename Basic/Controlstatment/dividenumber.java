bc1c85857e993267ffa634ddf3c293ceb3afa311
prem <premshekhawat19@gmail.com>
2024-12-12 14:44:05 +0530

    Number divede

---
+++ Basic/Controlstatment/dividenumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class dividenumber
+{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your number");
+        n=sc.nextInt();
+        if(n%5==0 && n%11==0)
+        {
+            System.out.println("Number is divisible by 11 and 5");
+        }
+        else
+        {
+            System.out.println("Numbe is not divisivle by 11 and 5");
+        }
+    }
+}
