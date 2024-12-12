cf798a61d5025ff60c692e407b7304e279c8be73
prem <premshekhawat19@gmail.com>
2024-12-12 15:23:02 +0530

    Week day

---
+++ Basic/Controlstatment/weekday.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class weekday
+{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your number");
+        n=sc.nextInt();
+        if(n==1)
+        {
+            System.out.println("Sunday");
+        }
+        else if(n==2)
+        {
+            System.out.println("Monday");
+        }
+        else if(n==3)
+        {
+            System.out.println("tuesday");
+        }
+        else if(n==4)
+        {
+            System.out.println("Wednesday");
+        }
+        else if(n==5)
+        {
+            System.out.println("Thrusday");
+        }
+        else if(n==6)
+        {
+            System.out.println("Friday");
+        }
+        else if(n==7)
+        {
+            System.out.println("Saturday");
+        }
+        else
+        {
+            System.out.println("Input not matched");
+        }
+    }
+}
