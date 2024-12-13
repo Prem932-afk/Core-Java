3666a0bc1d6fcf729cef7b4d651faf3db3180ac8
prem <premshekhawat19@gmail.com>
2024-12-13 14:51:30 +0530

    Grade

---
+++ Basic/Control Statment ASS-2/grade.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class grade
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int marks;
+        System.out.println("Enter your marks");
+        marks=sc.nextInt();
+        if(marks>1 && marks <=25)
+        {
+            System.out.println("Fail");
+        }
+        else if(marks>25 && marks<=45)
+        {
+            System.out.println("E grade");
+        }
+        else if(marks>45 && marks<=50)
+        {
+            System.out.println("D grade");
+        }
+        else if(marks>50 && marks<=60)
+        {
+            System.out.println("C grade");
+        }
+        else if(marks>60 && marks<=80)
+        {
+            System.out.println("B grade");
+        }
+        else if(marks>80 && marks<=100)
+        {
+            System.out.println("A grade");
+        }
+        else
+        {
+            System.out.println("Input not matched");
+        }
+    }
+
+}
