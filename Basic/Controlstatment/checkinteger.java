d08ce62c4c5ac78e7d266465d33d37409dacd055
prem <premshekhawat19@gmail.com>
2024-12-14 20:54:54 +0530

    uploading files

---
+++ Basic/Controlstatment/checkinteger.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class checkinteger
+{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your number");
+        n=sc.nextInt();
+        if(n)
+    }
+}
