8f4f6f3e6b426074a16497e0ea96a59d9ff7b927
prem <premshekhawat19@gmail.com>
2024-12-18 14:08:37 +0530

    uploading files

---
+++ Basic/OOP/No return & argument/Method4.java
@@ -0,0 +1,73 @@
+class Method4
+{
+    public static void main(String[] args) 
+    {
+    
+        A p=new A();
+        p.show(1);
+        B p1=new B();
+        p1.show2(2);
+        C p2=new C();
+        p2.show3(3);;
+        p2.show4(4);
+        p2.show5(5);
+        D p3=new D();
+        p3.show6(6);
+        p3.show7(7);;
+        p3.show8(8);
+        p3.show9(9);
+      
+        
+    }
+}
+class A
+{
+    void show(int a)
+    {
+        System.out.println(a);
+    }
+    
+}
+class B
+{
+    void show2(int a)
+    {
+        System.out.println(a);
+    }
+}
+class C
+{
+    void show3(int a)
+    {
+        System.out.println(a);
+    }
+    void show4(int a)
+    {
+        System.out.println(a);
+    }
+    void show5(int a)
+    {
+        System.out.println(a);
+    }
+    
+}
+class D
+{
+    void show6(int a)
+    {
+        System.out.println(a);
+    }
+    void show7(int a)
+    {
+        System.out.println(a);
+    }
+    void show8(int a)
+    {
+        System.out.println(a);
+    }
+    void show9(int a)
+    {
+        System.out.println(a);
+    }
+    
+}
