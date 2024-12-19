e1dd96fc7df8d88b1052877bb4a5379a2338de19
prem <premshekhawat19@gmail.com>
2024-12-19 13:00:05 +0530

    Celsius and convert it into Fahrenheit

---
+++ Basic/Loop's/100 Questions/Celsiutofahrenheit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Celsiutofahrenheit
+{
+    public static void main(String[] args)
+    {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter termpretor in Celsius :");
+       double celsius=sc.nextDouble();
+       
+       double fahrenheit=1.8*celsius+32;
+       System.out.println("Celsius to Fahrenheit :"+fahrenheit);
+    }
+}
