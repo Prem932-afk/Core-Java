36c996be958dcde9330c86809f8b5950f1d10a21
prem <premshekhawat19@gmail.com>
2024-12-19 12:38:15 +0530

    Area of rectangle

---
+++ Basic/Loop's/100 Questions/Arearectangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Arearectangle
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter rectangle length :");
+        double length=sc.nextDouble();
+        System.out.println("Enter rectangle breath :");
+        double breath=sc.nextDouble();
+        double area=length*breath;
+        System.out.println("Rectamge parameter : " + area);   
+    }
+}
