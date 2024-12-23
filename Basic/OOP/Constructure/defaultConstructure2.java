8d7a68aad8c71403367ff21e929dee5244d4ef21
prem <premshekhawat19@gmail.com>
2024-12-23 17:36:09 +0530

    Default Constrcutre : Write a program to create a class Person with a default constructor that initializes name to Unknown and age to 0.

---
+++ Basic/OOP/Constructure/defaultConstructure2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class defaultConstructure2 {
+    public static void main(String[] args) {
+        person p=new person();
+        System.out.println("Name :"+p.getName());
+        System.out.println("Age :"+p.getAge());
+
+    }
+}
+
+class person
+{
+    private String name;
+    private int age;
+    person()
+    {
+        this.name=name;
+        this.age=age;
+    }
+    
+    String getName()
+    {
+        return name;
+    }
+    
+    int getAge()
+    {
+        return age;
+    }
+}
