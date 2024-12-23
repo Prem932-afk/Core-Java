8ad20bf01f8cb6dc3fdfa1e03d34937a9436ef8b
prem <premshekhawat19@gmail.com>
2024-12-23 16:45:19 +0530

    Compund Intrest

---
+++ Basic/Loop's/100 Questions/CompundIntrest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class CompundIntrest{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your principle amount :");
+        double principle=sc.nextDouble();
+        System.out.println("Eneter the time in year ");
+        double time=sc.nextDouble();
+        System.out.println("Enter the intresst");
+        double rate=sc.nextDouble();
+
+        double amount=principle* Math.pow(1+rate/100,time);
+        double CompundIntrest=amount-principle;
+        System.out.println("Your Compund intrest (CI) :"+CompundIntrest);
+        System.out.println("Total Amount after " + time + " years is: " + amount);
+        sc.close();
+        
+    }
+}
+
