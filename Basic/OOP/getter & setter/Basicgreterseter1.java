12639c5c78c53f84015e1cc2ed30189c50176928
prem <premshekhawat19@gmail.com>
2024-12-23 20:17:34 +0530

    Greter seter person age & name

---
+++ Basic/OOP/getter & setter/Basicgreterseter1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Basicgreterseter1
+{
+    public static void main(String[] args) {
+        person p=new person("prem", 18);
+        System.out.println( "Person name :"+p.getName());
+        System.out.println("Person Age :"+p.getAge());
+    }
+}
+class person
+{
+          private String name;
+          private int age;
+          
+          person(String name, int age)
+          {
+                this.name=name;
+                this.age=age;
+          }
+
+          String getName()
+          {
+             return name;
+          }
+
+          int getAge()
+          {
+            return age;
+          }
+          
+
+}
