eb07987c5206c15f8dd17e081db438e08df6ca45
prem <premshekhawat19@gmail.com>
2024-12-23 16:36:14 +0530

    Simple Intrest

---
+++ Basic/Loop's/100 Questions/SimpleIntrest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class SimpleIntrest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your principle amount :");
+        double amount=sc.nextDouble();
+        System.out.println("Eneter the time year ");
+        double time=sc.nextDouble();
+        System.out.println("Enter the intresst");
+        double rate=sc.nextDouble();
+
+        double SimpleIntrest=(amount*time*rate)/100;
+        System.out.println("Your simple intrest value :"+SimpleIntrest);
+        sc.close();
+    }
+}
