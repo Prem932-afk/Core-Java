55d80e6fdb1420394f76c851221507754a513aa7
prem <premshekhawat19@gmail.com>
2024-12-23 21:49:18 +0530

    Method 3

---
+++ Basic/OOP/No return & argument/Method3.java
@@ -0,0 +1,73 @@
+class Method3
+{
+    public static void main(String[] args) 
+    {
+    
+        A p=new A();
+        p.show();
+        B p1=new B();
+        p1.show2();
+        C p2=new C();
+        p2.show3();;
+        p2.show4();
+        p2.show5();
+        D p3=new D();
+        p3.show6();
+        p3.show7();;
+        p3.show8();
+        p3.show9();
+      
+        
+    }
+}
+class A
+{
+    void show()
+    {
+        System.out.println("class: A ,method :Show");
+    }
+    
+}
+class B
+{
+    void show2()
+    {
+        System.out.println("class: B ,method : Show2");
+    }
+}
+class C
+{
+    void show3()
+    {
+        System.out.println("class: C ,method :Show3");
+    }
+    void show4()
+    {
+        System.out.println("class: C ,method :Show4");
+    }
+    void show5()
+    {
+        System.out.println("class: C ,method :Show5");
+    }
+    
+}
+class D
+{
+    void show6()
+    {
+        System.out.println("class: D ,method :Show6");
+    }
+    void show7()
+    {
+        System.out.println("class: D ,method :Show7");
+    }
+    void show8()
+    {
+        System.out.println("class: D ,method :Show8");
+    }
+    void show9()
+    {
+        System.out.println("class: D ,method :Show9");
+    }
+    
+}
