96c7ecf24086b8cb500b22e3e573fbef566b198d
prem singh <premshekhawat1997@gmail.com>
2024-12-24 16:36:57 +0530

    Method 3

---
+++ Basic/OOP/Inheritance/Method3.java
@@ -0,0 +1,28 @@
+class Method3 {
+ public static void main(String[] args) {
+    new B();
+    
+    
+ }   
+}
+class A
+{
+         A()
+         {
+            System.out.println("Constructure A");
+         }
+}
+class B extends A
+{
+         B()
+         {
+            System.out.println("Constructure B");
+         }
+}
+// A 
+// 	default constructor :print 
+
+// B 
+// 	default constructor :print 
+
+// access child through
