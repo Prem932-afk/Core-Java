1d3fdaa3a600e98b279125635e46a3e567c8768f
prem singh <premshekhawat1997@gmail.com>
2024-12-28 20:38:25 +0530

    Triangle validation

---
+++ Basic/Loop's/100 Questions/TriangleValidation.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TriangleValidation 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input angles of the triangle
+        System.out.print("Enter the first angle of the triangle: ");
+        int angle1 = scanner.nextInt();
+        
+        System.out.print("Enter the second angle of the triangle: ");
+        int angle2 = scanner.nextInt();
+        
+        System.out.print("Enter the third angle of the triangle: ");
+        int angle3 = scanner.nextInt();
+
+        // Check if the sum of the angles is 180 and each angle is positive
+        if (angle1 > 0 && angle2 > 0 && angle3 > 0 && (angle1 + angle2 + angle3 == 180))
+        {
+            System.out.println("The triangle is valid.");
+        } 
+        else 
+        {
+            System.out.println("The triangle is not valid.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
+
