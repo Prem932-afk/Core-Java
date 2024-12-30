6dafb0fa3bd0354ccd8a64addc1449a7b2652ad3
prem singh <premshekhawat1997@gmail.com>
2024-12-30 16:11:47 +0530

    Car class inherits from vehicle class

---
+++ Basic/OOP/Inheritance/Mehtod23.java
@@ -0,0 +1,29 @@
+public class Mehtod23 {
+    public static void main(String[] args) {
+        Car c=new Car();
+        c.vehicle(80, "Kia");
+        c.car(4);
+    }
+}
+class Vehicle
+{
+    int speed;
+    String brand;
+    void vehicle(int speed, String brand)
+    {
+        this.speed=speed;
+        this.brand=brand;
+        System.out.println("speed :"+speed);
+        System.out.println("brand"+brand);
+    }
+}
+class Car extends Vehicle
+{
+      int doors;
+      void car(int doors)
+      {
+        this.doors=doors;
+        System.out.println("Doors :"+doors);
+      }
+}
+//Implement a program where a Car class inherits from a Vehicle class, and call the inherited method in the subclass.
