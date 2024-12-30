353179dde8170b577b67cb0ede49f20cb865f8f3
prem singh <premshekhawat1997@gmail.com>
2024-12-30 17:03:13 +0530

    Methdo 4

---
+++ Basic/OOP/retrun & No argument/Method4.java
@@ -0,0 +1,94 @@
+class Method4
+{
+    public static void main(String[] args) {
+        A a=new A()
+         a.show1(1);
+        B b=new B();
+         b.show2(2);
+        C c=new C();
+         c.show3(3);
+         c.show4(4);
+         c.show5(5);
+         c.show6(6);
+        D d=new D();
+         d.show7(7);
+         d.show8(8);
+         d.show9(9);
+         d.show10(10);
+    }
+}
+class A
+{
+    int show1(int a)
+    {
+       
+        System.out.println("A value"+a);
+        return a;
+    }
+}
+class B
+{
+    int show2(int b)
+    {
+       
+        System.out.println("b value"+b);
+        return b;
+    }
+}
+class C
+{
+    int show3(int c)
+    {
+       
+        System.out.println("c Value"+c);
+        return c;
+    }
+    int show4(int D)
+    {
+       
+        System.out.println("D Value"+D);
+        return D;
+    }
+    int show5(int E)
+    {
+       
+        System.out.println("E Value"+E);
+        return E;
+    }
+    int show6(int F)
+    {
+       
+        System.out.println("F Value"+F);
+        return F;
+    }
+    
+}
+class D
+{
+    int show7(int G)
+    {
+       
+        System.out.println("G Value :"+G);
+        return G;
+    }
+    int show8(int H)
+    {
+       
+        System.out.println("H Value"+H);
+        return H;
+    } 
+    int show9(int I)
+    {
+       
+        System.out.println("I Value"+I);
+        return I;
+    } 
+    int show10(int J)
+    {
+       
+        System.out.println("J Value"+J);
+        return J;
+    }
+}
+
+
