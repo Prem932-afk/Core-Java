2d97c34944c2c8556d5ccbd8d6664388d6840ace
prem singh <premshekhawat1997@gmail.com>
2025-01-06 19:58:42 +0530

    private & protected

---
+++ Basic/OOP/Polymophism/Runtime polumphirsm/method24.java
@@ -0,0 +1,40 @@
+public class method24 {
+
+    public static void main(String[] args) {
+        B b=new B();
+        b.antother1();
+       b.method2();
+       
+}
+}
+class A
+{
+         private void method1()
+         {
+            System.out.println("method 1");
+         }
+         void antother1()
+         {
+             
+                 A a=new A();
+                 a.method1();//first way
+                 method1();//second way
+                 
+         }
+
+
+}
+        
+class B extends A
+{
+    protected void method2()
+    {
+       
+       System.out.println("method 2");
+    }
+
+}
+
+
+
+
