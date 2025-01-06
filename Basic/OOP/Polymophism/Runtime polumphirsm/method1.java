b6569f07042256c4a582314d7d0bba0a05f116e0
prem singh <premshekhawat1997@gmail.com>
2025-01-06 17:03:08 +0530

    class name different but method name same

--- Basic/OOP/Polymophism/Runtime polumphirsm/method1.java
+++ Basic/OOP/Polymophism/Runtime polumphirsm/method1.java
@@ -1,44 +1,22 @@
-class method1{
-    public static void main(String[] args) {
-        D d=new D();
-        d.show(3);
-        d.show(2,4);
-        
-    }
+class method1
+{
+ public static void main(String[] args) {
+    B b=new B();
+    b.method2();
+    b.method1();
+ }
 }
 class A
 {
-         void show()
-         {
-            System.out.println("void show");
-         }
-         
+               void method1()
+               {
+                System.out.println("first method");
+               }
 }
 class B extends A
 {
-         void show()
-         {
-             super.show();
-             System.out.println("void show 2");
-         }
+    void method2()
+    {
+     System.out.println("second method");
+    }
 }
-class C extends B
-{
-     void show(int a,int b)
-     {
-        System.out.println("void show 3");
-     }
-}
-class D extends C
-{
-      int show(int a)
-      {
-        
-        return a;
-      }
-}
-
-//class name different and method name same then method will be overrided 
-//class name different and method name same child k through parent method ko call krna hai used super keyword 
-//class name different and method name same number of parameter :
-//class name different method name same but return type of method changed 
