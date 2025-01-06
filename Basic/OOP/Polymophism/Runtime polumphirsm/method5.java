72202e8613ad37e72aab1dbc9311a4af795b5c8c
prem singh <premshekhawat1997@gmail.com>
2025-01-06 18:01:45 +0530

    multilevel inheritence method overriding

---
+++ Basic/OOP/Polymophism/Runtime polumphirsm/method5.java
@@ -0,0 +1,30 @@
+public class method5 {
+    
+  
+     public static void main(String[] args) {
+        c c=new c();
+        c.method();
+      
+     }
+    }
+    class A
+    {
+        void method()
+        {
+            System.out.println("first method");
+        }
+    }
+    class B extends A
+    {
+        void method()
+        {
+         System.out.println("second method");
+        }
+    }
+    class c extends B
+    {
+        void method()
+        {
+         System.out.println("third method");
+        }
+    }
