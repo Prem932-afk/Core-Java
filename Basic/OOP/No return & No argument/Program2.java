a8db21a17ab30c54b4d71e8da9678129fb46f329
prem singh <premshekhawat1997@gmail.com>
2025-01-08 17:28:05 +0530

    Method 1 & Method 2

---
+++ Basic/OOP/No return & No argument/Program2.java
@@ -0,0 +1,25 @@
+public class Program2 {
+    public static void main(String[] args) {
+        A a=new A();
+         a.method1();
+         a.method2();
+    }
+}
+class A
+{
+    void method1()
+    {
+        System.out.println("Method 1");
+    }
+
+    void method2()
+    {
+        System.out.println("Method 2");
+    }
+}
+
+
+// A :
+// 	method1 :
+// 	method2 :
+	
