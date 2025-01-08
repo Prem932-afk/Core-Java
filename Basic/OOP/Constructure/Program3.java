78c540440128aa7d1f0abdaec45d5a184acd155e
prem singh <premshekhawat1997@gmail.com>
2025-01-08 20:55:42 +0530

    Two number addition through instance variabel

---
+++ Basic/OOP/Constructure/Program3.java
@@ -0,0 +1,23 @@
+public class Program3 
+{
+    
+    public static void main(String[] args) 
+    {
+        new A(2, 07);
+      
+    }
+}
+class A
+{
+    int a,b;
+    A(int a, int b)
+    {
+        this.a=a;
+        this.b=b;
+        int c=a+b;
+        System.out.println("Addition of two value : "+c);
+    }
+}
+//two number addition through instance variable way :constant 
+
+
