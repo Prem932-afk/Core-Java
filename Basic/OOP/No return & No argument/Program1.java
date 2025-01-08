a5b2806b7d7371f441de8428b1eba31b16145c43
prem singh <premshekhawat1997@gmail.com>
2025-01-08 17:57:39 +0530

    class A: print (Hello World)

---
+++ Basic/OOP/No return & No argument/Program1.java
@@ -0,0 +1,21 @@
+public class Program1 
+{
+    public static void main(String[] args) 
+    {
+           A a=new A();
+           a. show();
+    }
+}
+
+class A
+{
+    void show()
+    {
+         System.out.println("Hello World");
+    }
+}
+
+
+// A :
+// 	method 1 :hello world
+	
