f0ea74b867950e9222d1b8850af4bf9076705119
prem singh <premshekhawat1997@gmail.com>
2025-01-08 17:35:46 +0530

    class 1 : Method 1, class 2: method 1 class 3: method 4, class 3: method 4

---
+++ Basic/OOP/No return & No argument/Program3.java
@@ -0,0 +1,83 @@
+public class Program3 
+{
+ public static void main(String[] args) 
+ {
+     A a=new A();
+      a.method1();
+    
+     B b=new B();
+      b.method2();
+
+     C c=new C();
+      c.method3();
+      c.method4();
+      c.method5();
+      
+     D d=new D();
+      d.method6();
+      d.method7();
+      d.method8();
+      d.method9();
+ }    
+}
+class A
+{
+    void method1()
+    {
+        System.out.println("Method 1");
+    }
+}
+class B
+{
+    void method2()
+    {
+        System.out.println("Method 2");
+    }
+}
+class C
+{
+    void method3()
+    {
+        System.out.println("Method 3");
+    }
+
+    void method4()
+    {
+        System.out.println("Mehtod4");
+    }
+    void method5()
+    {
+        System.out.println("Method 5");
+    }
+   
+}
+class D
+{
+    void method6()
+    {
+        System.out.println("Method 6");
+    }
+
+    void method7()
+    {
+        System.out.println("Mehtod 7");
+    }
+    void method8()
+    {
+        System.out.println("Method 8");
+    }
+    void method9()
+    {
+        System.out.println("Method 9");
+    }
+}
+
+// A :
+// 	method one
+// B:
+// 	method one
+// C:
+// 	method three
+// D:
+// 	method four
+	
