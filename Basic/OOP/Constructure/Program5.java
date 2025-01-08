2df5624b9d9e26635818ba4ff67c93d4ff971483
prem singh <premshekhawat1997@gmail.com>
2025-01-08 22:43:29 +0530

    user input of instance variable through parameterised constructor and parameter name is also different

---
+++ Basic/OOP/Constructure/Program5.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+public class Program5 
+    
+{
+   public static void main(String[] args) 
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter your number :");
+     int x=sc.nextInt();
+     int y=sc.nextInt();
+
+     A p=new A(x,y);
+      p.add();
+      p.sub();
+      p.mul();
+      p.div();
+
+      
+   }    
+}
+class A
+{
+    int a;
+    int b;
+
+    A(int x,int y)
+    {
+          a=x;
+          b=y;
+          
+    }
+
+    
+    void add()
+    {
+        
+        int c=a+b;
+        System.out.println("Addition :"+c);
+    }
+
+    void sub()
+    {
+        
+        int c=a-b;
+        System.out.println("Substracton :"+c);
+    }
+
+    void mul()
+    {
+          
+          int c=a*b;
+          System.out.println("Multiple :"+c);
+    }
+
+    void div()
+    {
+          
+          int c=a/b;
+          System.out.println("Divison :"+c);
+    }   
+
+}
+
+
+//user input of instance variable through parameterised constructor and parameter name is also different 
+// A :addition
+
+// B :sub
+
+// C :multi
+
+// D:div
