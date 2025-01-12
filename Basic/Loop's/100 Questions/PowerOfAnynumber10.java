09495a0e40b455db53c75a04ec7f67457313f603
prem singh <premshekhawat1997@gmail.com>
2025-01-12 11:29:10 +0530

    find power of any number x ^ y.

---
+++ Basic/Loop's/100 Questions/PowerOfAnynumber10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PowerOfAnynumber10 
+{
+   public static void main(String[] args) 
+   {
+      int result=1;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter your number");
+      int n=sc.nextInt();
+      
+      System.out.println("Enter your power");
+      int p=sc.nextInt();
+      for(int i=1; i<=p;i++)
+      {
+         result=result*n;
+         System.out.println(result);
+      }
+   }    
+}
