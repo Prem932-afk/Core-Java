f5caa279d1a3a96f959b60ee85430a49e12c0981
prem singh <premshekhawat1997@gmail.com>
2025-01-12 11:31:42 +0530

    Square Root

---
+++ Basic/Loop's/100 Questions/SquareRoot11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SquareRoot11 
+{
+  public static void main(String[] args) 
+  {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your number");
+        int n=sc.nextInt();
+        double m=Math.sqrt(n);
+        System.out.println(n+"  is  " +m);
+  }   
+}
+
+//Write a java program to enter any number and calculate its square root.
