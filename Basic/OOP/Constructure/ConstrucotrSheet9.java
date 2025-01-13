2f6fe4a0775c7e399e34f0896abffb779fe714fb
prem singh <premshekhawat1997@gmail.com>
2025-01-13 12:36:02 +0530

    a class Book with a constructor that accepts a String representing the book’s title and assigns it to the title instance variable.

---
+++ Basic/OOP/Constructure/ConstrucotrSheet9.java
@@ -0,0 +1,19 @@
+public class ConstrucotrSheet9 
+{
+   public static void main(String[] args) 
+   {
+      Book b=new Book("Java");
+   }    
+}
+class Book
+{
+     String title;
+     Book(String title)
+     {
+        this.title=title;
+        System.out.println("Book Title :"+title);
+     }
+}
+//Constructor with String Parameter:
+
+//Define a class Book with a constructor that accepts a String representing the book’s title and assigns it to the title instance variable.
