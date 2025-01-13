5efe93efdcb3b74c5d39a9662f308a5b40afe7e7
prem singh <premshekhawat1997@gmail.com>
2025-01-13 10:30:46 +0530

    Maximum between Three Number

---
+++ Basic/Loop's/100 Questions/MaxThreeNumber18.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MaxThreeNumber18 
+{
+   public static void main(String[] args) 
+   {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter ther frist Number :");
+        int a=sc.nextInt();
+        System.out.println("Enter the Second Number :");
+        int b=sc.nextInt();
+        System.out.println("Enter the Third Number :");
+        int c=sc.nextInt();
+        if(a>b && a>c)
+        {
+            System.out.println("First Number is Maximum");
+        }
+        else if(b>a && b>c)
+        {
+            System.out.println("Second Number is Maxium");
+        }
+        else
+        {
+            System.out.println("Third Number is Maximum");
+        }
+        sc.close();
+   }    
+}
+//Write a java program to find Maximum three Number ?
