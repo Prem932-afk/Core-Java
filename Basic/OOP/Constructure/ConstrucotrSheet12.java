5be62d305e5e30233cb18e24a97b09515c569fb8
prem singh <premshekhawat1997@gmail.com>
2025-01-15 13:45:34 +0530

    a Student class with instance variables name and marks. Write a constructor that initializes name to Unknown and marks to 0 by default.

---
+++ Basic/OOP/Constructure/ConstrucotrSheet12.java
@@ -0,0 +1,20 @@
+public class ConstrucotrSheet12 
+{
+    public static void main(String[] args) 
+    {
+        new Student();
+    }    
+}
+class Student 
+{
+        int marks;
+        String name;
+        Student()
+        {
+            this.marks=0;
+            this.name="Java";
+        }
+}
+//Constructor with Default Values:
+
+//Create a Student class with instance variables name and marks. Write a constructor that initializes name to "Unknown" and marks to 0 by default.
