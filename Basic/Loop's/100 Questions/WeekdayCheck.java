b435f01d3e4b1e57fc6100c1a67729bbaf5844d9
prem singh <premshekhawat1997@gmail.com>
2025-01-18 14:22:12 +0530

    Write a java program to input week number and print week day

--- Basic/Loop's/100 Questions/WeekdayCheck.java
+++ Basic/Loop's/100 Questions/WeekdayCheck.java
@@ -1,7 +1,9 @@
-import java.util.Scanner;
 
-public class WeekdayCheck {
-    public static void main(String[] args) {
+import java.util.Scanner;
+public class WeekdayCheck 
+{
+    public static void main(String[] args) 
+    {
         // Create a Scanner object to take input
         Scanner scanner = new Scanner(System.in);
         
@@ -10,21 +12,36 @@ public class WeekdayCheck {
         int weekNumber = scanner.nextInt();
         
         // Check the week number and print the corresponding weekday using if-else conditions
-        if (weekNumber == 1) {
+        if (weekNumber == 1) 
+        {
             System.out.println("Day 1 is Sunday.");
-        } else if (weekNumber == 2) {
+        } 
+        else if (weekNumber == 2) 
+        {
             System.out.println("Day 2 is Monday.");
-        } else if (weekNumber == 3) {
+        } 
+        else if (weekNumber == 3) 
+        {
             System.out.println("Day 3 is Tuesday.");
-        } else if (weekNumber == 4) {
+        } 
+        else if (weekNumber == 4) 
+        {
             System.out.println("Day 4 is Wednesday.");
-        } else if (weekNumber == 5) {
+        } 
+        else if (weekNumber == 5) 
+        {
             System.out.println("Day 5 is Thursday.");
-        } else if (weekNumber == 6) {
+        } 
+        else if (weekNumber == 6) 
+        {
             System.out.println("Day 6 is Friday.");
-        } else if (weekNumber == 7) {
+        } 
+        else if (weekNumber == 7) 
+        {
             System.out.println("Day 7 is Saturday.");
-        } else {
+        } 
+        else 
+        {
             System.out.println("Invalid week number! Please enter a number between 1 and 7.");
         }
         
@@ -32,3 +49,6 @@ public class WeekdayCheck {
         scanner.close();
     }
 }
+
+
+//Write a java program to input week number and print week day ?
