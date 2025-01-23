a01089901b6473c0cd443b8f3a3df7a7d5c5ac67
prem singh <premshekhawat1997@gmail.com>
2025-01-23 20:18:52 +0530

    a java program to print all even numbers between 1 to 100. - using while loop.

---
+++ Basic/Loop's/100 Questions/EvenNumbers.java
@@ -0,0 +1,17 @@
+
+public class EvenNumbers 
+{
+    public static void main(String[] args) 
+    {
+        // Initialize the number to 2, as it's the first even number
+        int num = 2;
+
+        // While loop to print even numbers between 1 and 100
+        while (num <= 100) 
+        {
+            System.out.print(num + " ");
+            num += 2; // Increment by 2 to get the next even number
+        }
+    }
+}
+//Write a java program to print all even numbers between 1 to 100. - using while loop.
