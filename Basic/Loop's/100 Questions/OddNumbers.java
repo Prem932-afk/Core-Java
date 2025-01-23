4e810618bb03efccd0422ecc872cb48dde857641
prem singh <premshekhawat1997@gmail.com>
2025-01-23 20:20:58 +0530

    a java program to print all odd number between 1 to 100.

---
+++ Basic/Loop's/100 Questions/OddNumbers.java
@@ -0,0 +1,16 @@
+public class OddNumbers 
+{
+    public static void main(String[] args) 
+    {
+        // Initialize the number to 1, as it's the first odd number
+        int num = 1;
+
+        // While loop to print odd numbers between 1 and 100
+        while (num <= 100) 
+        {
+            System.out.print(num + " ");
+            num += 2; // Increment by 2 to get the next odd number
+        }
+    }
+}
+//Write a java program to print all odd number between 1 to 100.
