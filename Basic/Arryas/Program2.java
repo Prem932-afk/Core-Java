48f909b95281b8690d4da8316b9b7e69837d6156
prem singh <premshekhawat1997@gmail.com>
2025-02-10 18:12:30 +0530

    Name string print

---
+++ Basic/Arryas/Program2.java
@@ -0,0 +1,11 @@
+public class Program2 
+{
+  public static void main(String[] args) 
+  {
+    String name []={"Prem", "Rahul","Rohit", "Yashu","Virat"};  
+    for(int i=0; i<name.length;i++)
+    {
+        System.out.println(name[i]);
+    }
+  }    
+}
