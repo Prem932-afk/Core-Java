1d15679d003b2534166a4f373a3b72b7bdc01c49
prem singh <premshekhawat1997@gmail.com>
2025-02-10 18:47:49 +0530

    Print arrays

---
+++ Basic/Arryas/Program12.java
@@ -0,0 +1,16 @@
+public class Program12 {
+    public static void main(String[] args) 
+    {
+      int[] arr = {1,2,3,4,5,6,7,8,9,10};
+          
+      
+      int[] secondArr = {1,2,3,4,5,6,8,10  };
+  
+     
+      System.out.print("Array: ");
+      for (int i = 0; i < secondArr.length; i++) 
+      {
+          System.out.print(secondArr[i] + " ");
+      }
+    }  
+}
