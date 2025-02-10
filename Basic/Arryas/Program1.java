91fe714d3ea71be4c62ca8e89a601a0960d1ea7d
prem singh <premshekhawat1997@gmail.com>
2025-02-10 18:08:33 +0530

    Arrays lenght

---
+++ Basic/Arryas/Program1.java
@@ -0,0 +1,10 @@
+class Program1
+{
+    public static void main(String[] args) 
+    {
+      
+      int a[]={12,67,46,435,67,385,335,335,34};
+      int n=a.length;
+      System.out.println(n); 
+    }
+}
