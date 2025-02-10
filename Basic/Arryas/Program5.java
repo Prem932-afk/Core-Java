e6a3be0a5887f503efebcc5cbe4e029656c3e393
prem singh <premshekhawat1997@gmail.com>
2025-02-10 18:23:31 +0530

    Mid Element

---
+++ Basic/Arryas/Program5.java
@@ -0,0 +1,10 @@
+class Program5
+{
+     public static void main(String[] args) 
+     {
+         int a[]={12,67,46,435,67,385,335,335,34};   
+         int length=a.length;
+         int mid=length/2;
+         System.out.println(a[mid]);
+     }
+}
