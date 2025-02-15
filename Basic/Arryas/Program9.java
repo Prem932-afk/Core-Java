4d82975d682059778d026c144ac9ed1ad40f24e2
prem singh <premshekhawat1997@gmail.com>
2025-02-15 15:50:28 +0530

    Reordered array

--- Basic/Arryas/Program9.java
+++ Basic/Arryas/Program9.java
@@ -1,16 +1,33 @@
-public class Program9 {
-    public static void main(String[] args) 
+public class Program9 
+{
+  public static void main(String[] args) 
+  {
+   
+    int[] arr = {12, 67, 46, 435, 607, 385, 33, 335, 34, 23};
+
+   
+    int[] result = new int[arr.length];
+
+    
+    result[0] = arr[4];  // 607
+    result[1] = arr[3];  // 435
+    result[2] = arr[2];  // 46
+    result[3] = arr[1];  // 67
+    result[4] = arr[0];  // 12
+    result[5] = arr[9];  // 23
+    result[6] = arr[8];  // 34
+    result[7] = arr[7];  // 335
+    result[8] = arr[6];  // 33
+    result[9] = arr[5];  // 385
+
+    
+    System.out.println("Reordered Array: ");
+    for (int num : result) 
     {
-      int[] arr = {12,67,46,435,607,385,33,335,34,23};
-          
-      
-      int[] secondArr = {607,435,46,67,12,23,34,335,33,385 };
-  
-     
-      System.out.print("Array: ");
-      for (int i = 0; i < secondArr.length; i++) 
-      {
-          System.out.print(secondArr[i] + " ");
-      }
-    }  
+        System.out.print(num + " ");
+    }
 }
+
+}
+// output:
+// 607,435,46,67,12,385,33,335,34,23
