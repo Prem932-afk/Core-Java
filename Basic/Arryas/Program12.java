42226926d82c3a2de857897e80c0db030a77260c
prem singh <premshekhawat1997@gmail.com>
2025-02-15 15:59:02 +0530

    Reordered array

--- Basic/Arryas/Program12.java
+++ Basic/Arryas/Program12.java
@@ -1,16 +1,18 @@
-public class Program12 {
+public class Program12 
+{
     public static void main(String[] args) 
     {
-      int[] arr = {1,2,3,4,5,6,7,8,9,10};
-          
-      
-      int[] secondArr = {1,2,3,4,5,6,8,10  };
-  
-     
-      System.out.print("Array: ");
-      for (int i = 0; i < secondArr.length; i++) 
-      {
-          System.out.print(secondArr[i] + " ");
-      }
+      int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        
+        System.out.println("Reordered Array: ");
+        for (int i = 0; i < arr.length; i++) 
+        {
+            // Only print the elements that are not 7 and 9
+            if (arr[i] != 7 && arr[i] != 9) 
+            {
+                System.out.print(arr[i] + " ");
+            }
+        }
     }  
 }
