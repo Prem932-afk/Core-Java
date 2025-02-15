768fd38d2d4a76a502776e2d7c781a16b853bb31
prem singh <premshekhawat1997@gmail.com>
2025-02-15 15:37:53 +0530

    Rorderd arry

--- Basic/Arryas/Program7.java
+++ Basic/Arryas/Program7.java
@@ -3,15 +3,42 @@ public class Program7
   public static void main(String[] args) 
   {
     int[] arr = {12, 67, 46, 435, 67, 385, 33, 335, 34, 23};
-        
-    
-    int[] secondArr = {12, 67, 46, 435, 67, 23, 34, 335, 33, 385};
 
-   
-    System.out.print("Array: ");
-    for (int i = 0; i < secondArr.length; i++) 
+    int result[]=new int[arr.length]; 
+
+    for (int i = 0; i < arr.length; i++) 
     {
-        System.out.print(secondArr[i] + " ");
+         result[i]= arr[i];
     }
+
+      if (arr[5] != 23) 
+      {
+        result[5] = 23;
+      }
+      if (arr[6] != 34) 
+      {
+        result[6] = 34;
+      }
+
+      if (arr[7] != 335) 
+      {
+        result[7] = 335;
+      }
+
+      if (arr[8] != 33) 
+      {
+        result[8] = 33;
+      }
+
+      if (arr[9] != 385) 
+      {
+        result[9] = 385;
+      }
+
+      System.out.println("Reordered arry");
+      for(int num :result)
+      {
+         System.out.println(num);
+      }
   }    
 }
