95908c3b715e236737c4513de2bdf4b0c4094a75
prem singh <premshekhawat1997@gmail.com>
2025-02-16 12:32:28 +0530

    Array reorder

---
+++ Basic/Arryas/Assignment1to13.java
@@ -0,0 +1,13 @@
+public class Assignment1to13 {
+    public static void main(String[] args) {
+        // Create the array
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        // Loop through the array and use an if condition to check for the required values
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 2 || array[i] == 4 || array[i] == 7 || array[i] == 9) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
