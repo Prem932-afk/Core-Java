51b306e28a2b75ac806aefc2bbea8490b1282a70
prem singh <premshekhawat1997@gmail.com>
2025-02-24 18:55:30 +0530

    No reutn & No argument

---
+++ Basic/All-Question/P1Noreturn&Noargument.java
@@ -0,0 +1,15 @@
+class p
+{
+    public static void main(String[] args) 
+    {
+        Function f=new Function();
+        f.method();
+    }
+}
+class Function
+{
+    void method()
+    {
+        System.out.println("No return & No Arguments method");
+    }
+}
