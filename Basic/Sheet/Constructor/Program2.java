f3fa941319466733ed4d623191b92d857a0002d1
prem singh <premshekhawat1997@gmail.com>
2025-02-25 17:40:55 +0530

    // Write a program to create a class Person with a default constructor that initializes name to

---
+++ Basic/Sheet/Constructor/Program2.java
@@ -0,0 +1,20 @@
+public class Program2 
+{
+  public static void main(String[] args) 
+  {
+       new Person();  
+  }    
+}
+class Person
+{
+     String name="Unknow";
+     int age=0;
+     Person()
+     {
+        System.out.println("Name :"+name);
+        System.out.println("Age :"+age);
+     }
+}
+//Default Constructor:
+
+// Write a program to create a class Person with a default constructor that initializes name to "Unknown" and age to 0.
