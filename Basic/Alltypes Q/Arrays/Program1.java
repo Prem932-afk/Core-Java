f6c62346eb64f455a69a9f91cc51909f91309215
prem singh <premshekhawat1997@gmail.com>
2025-03-03 16:06:36 +0530

    Length find

---
+++ Basic/Alltypes Q/Arrays/Program1.java
@@ -0,0 +1,11 @@
+//Q.1 Lenght find 12,67,46,435,67,385,335,335,34
+
+class Program1
+{
+     public static void main(String[] args) 
+     {
+        int arr[]={12,67,46,435,67,385,335,335,34};   
+        int n=arr.length;
+        System.out.println("This array lenght:" + n);
+     }
+}
