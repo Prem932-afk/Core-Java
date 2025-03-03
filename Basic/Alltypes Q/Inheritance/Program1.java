6de713f783beebca5ab19249c8c5152a88241711
prem singh <premshekhawat1997@gmail.com>
2025-03-03 14:54:41 +0530

    Write a Java program to demonstrate single-level inheritance where a Student class inherits from a Person class.

---
+++ Basic/Alltypes Q/Inheritance/Program1.java
@@ -0,0 +1,42 @@
+//Q.1 Write a Java program to demonstrate single-level inheritance where a Student class inherits from a Person class.
+
+class Program1
+{
+    public static void main(String args[])
+    {
+           Student s=new Student("3344", "Prem", 23);
+           s.displyStudentdetails();
+    }
+}
+class Person
+{
+     String name;
+     int age;
+     Person(String name, int age)
+     {
+        this.name=name;
+        this.age=age;
+     }
+
+     void displydetails()
+     {
+        System.out.println("Person name :"+name);
+        System.out.println("Person age :"+age);
+     }
+}
+class Student extends Person
+{
+    
+      String id;
+      Student(String id,String name,int age)
+     {
+       super(name,age);
+       this.id=id;
+     }
+
+     void displyStudentdetails()
+     {
+         displydetails();
+         System.out.println("Student id"+id);
+     }
+}
