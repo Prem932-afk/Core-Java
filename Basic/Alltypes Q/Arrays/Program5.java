6fb604f881341a5c61f16bee32dea17c672c4e48
prem singh <premshekhawat1997@gmail.com>
2025-03-03 16:14:21 +0530

    Mid Element find 12,67,46,435,67,385,335,335,34

---
+++ Basic/Alltypes Q/Arrays/Program5.java
@@ -0,0 +1,19 @@
+/* 
+Q.5
+ 
+12,67,46,435,67,385,335,335,34
+
+mid element
+*/
+
+ 
+ class Program5
+{
+     public static void main(String[] args) 
+     {
+         int a[]={12,67,46,435,67,385,335,335,34};   
+         int length=a.length;
+         int mid=length/2;
+         System.out.println(a[mid]);
+     }
+}
