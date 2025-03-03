8ff1cc0c32ce0b5d6347ee31ee27b49bda5bfcb6
prem singh <premshekhawat1997@gmail.com>
2025-03-03 16:11:32 +0530

    Forwar Direction

---
+++ Basic/Alltypes Q/Arrays/Program3.java
@@ -0,0 +1,19 @@
+/* 
+Q.3 
+ 
+12,67,46,435,67,385,335,335,34
+
+forward direction 
+*/
+
+public class Program3 
+{
+  public static void main(String[] args) 
+  {
+       int a[]={12,67,46,435,67,385,335,335,34};  
+       for(int i=0; i<a.length; i++)
+       {
+           System.out.println(a[i]);
+       }
+  }    
+}
