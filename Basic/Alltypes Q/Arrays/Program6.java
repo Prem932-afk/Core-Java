e765fb374bbf88ddc6ad7832f63b55edc3a8536c
prem singh <premshekhawat1997@gmail.com>
2025-03-06 17:07:37 +0530

    Find first and last element of array

---
+++ Basic/Alltypes Q/Arrays/Program6.java
@@ -0,0 +1,15 @@
+// 12,67,46,435,67,385,335,335,34
+
+// first element and last element find 
+
+public class Program6 
+{
+  public static void main(String[] args) 
+  {
+    int a[]={12,67,46,435,67,385,335,335,34};   
+    int firstelement=a[0];
+    int lastelement= a[a.length-1];
+    System.out.println(firstelement);
+    System.out.println(lastelement);
+   }    
+}
