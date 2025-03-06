2970562e861cc31e8c980e6f2fa986827c04f6ae
prem singh <premshekhawat1997@gmail.com>
2025-03-06 16:48:42 +0530

    Write a java program to enter temperature in Celsius and convert it into Fahrenheit

---
+++ Basic/Alltypes Q/Basics Program/Program8.java
@@ -0,0 +1,17 @@
+ 
+import java.util.Scanner;
+public class Program8
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your tempretor in celcius :");
+        double celcius=sc.nextDouble();
+        //°F = (°C × 9/5) + 32
+        double fahrenheit=(celcius*9/5)+32;
+        System.out.println(fahrenheit);
+        
+    }
+}
+//Write a java program to enter temperature in Celsius and convert it into Fahrenheit
+//°F = (°C × 9/5) + 32
