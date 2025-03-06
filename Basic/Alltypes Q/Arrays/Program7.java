5c116f3803123f2ca213d31c2fb2928bd753206a
prem singh <premshekhawat1997@gmail.com>
2025-03-06 17:09:38 +0530

    array output show 12,67,46,435,67,23,34,335,33,385

---
+++ Basic/Alltypes Q/Arrays/Program7.java
@@ -0,0 +1,25 @@
+// Q.7
+ 
+// 12,67,46,435,67,385,33,335,34,23
+
+// output:
+// 12,67,46,435,67,23,34,335,33,385 
+
+
+public class Program7
+{
+  public static void main(String[] args) 
+  {
+    int[] arr = {12, 67, 46, 435, 67, 385, 33, 335, 34, 23};
+        
+    
+    int[] secondArr = {12, 67, 46, 435, 67, 23, 34, 335, 33, 385};
+
+   
+    System.out.print("Array: ");
+    for (int i = 0; i < secondArr.length; i++) 
+    {
+        System.out.print(secondArr[i] + " ");
+    }
+  }    
+}
