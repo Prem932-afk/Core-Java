9a20acc83463ad1c6692d65e7e01d49980811cc9
prem singh <premshekhawat1997@gmail.com>
2025-03-06 17:10:51 +0530

    Array output

---
+++ Basic/Alltypes Q/Arrays/Program8.java
@@ -0,0 +1,24 @@
+// Q.8
+ 
+// 12,67,46,435,607,385,33,335,34,23
+
+// output:
+// 607,435,46,67,12,385,33,335,34,23
+
+public class Program8 
+{
+    public static void main(String[] args) 
+    {
+      int[] arr = {12,67,46,435,607,385,33,335,34,23};
+          
+      
+      int[] secondArr = {607,435,46,67,12,385,33,335,34,23};
+  
+     
+      System.out.print("Array: ");
+      for (int i = 0; i < secondArr.length; i++) 
+      {
+          System.out.print(secondArr[i] + " ");
+      }
+    }  
+}
