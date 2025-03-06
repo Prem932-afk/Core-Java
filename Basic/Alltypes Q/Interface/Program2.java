2d2726b469aa6208f877a6a694904b33335e3224
prem singh <premshekhawat1997@gmail.com>
2025-03-06 16:28:51 +0530

    Create an interface Printable with a default method print(). Implement this interface in a class and override the print() method to customize its behavior.

---
+++ Basic/Alltypes Q/Interface/Program2.java
@@ -0,0 +1,26 @@
+
+// Q.2
+ 
+// Create an interface Printable with a default method print(). Implement this interface in a class and override the print() method to customize its behavior.
+
+
+
+public class Program2 
+{
+  public static void main(String[] args) 
+  {
+     Document d=new Document();
+     d.print();
+  }    
+}
+interface Printable
+{
+     void print();
+}
+class Document implements Printable
+{
+    public void print()
+   {
+    System.out.println("Printing the document with custom behavior.");
+   }
+}
