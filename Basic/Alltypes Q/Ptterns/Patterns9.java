b77c935614b7e3138a2c7a67f49a08157ae0ffd5
prem singh <premshekhawat1997@gmail.com>
2025-03-08 15:56:17 +0530

    Patterns9.java

---
+++ Basic/Alltypes Q/Ptterns/Patterns9.java
@@ -0,0 +1,18 @@
+public class Patterns9 
+{
+  public static void main(String[] args) 
+  {
+    for(int i=1; i<=5; i++)
+    {
+      for(int j=1; j<=i; j++)
+      {
+          System.out.print(i);
+      }
+      System.out.println(" ");
+    }   
+  }    
+}
+// 1
+// 22
+// 333
+// 4444 
