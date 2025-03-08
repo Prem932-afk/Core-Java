c31501bc86bc4e465985c4a08939dfd4f191c8ad
prem singh <premshekhawat1997@gmail.com>
2025-03-08 15:22:35 +0530

    Write a java program to find power of any number x ^ y.

---
+++ Basic/Alltypes Q/Basics Program/Program11.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Program11
+{
+   public static void main(String[] args) 
+   {
+      int result=1;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter your number");
+      int n=sc.nextInt();
+
+      System.out.println("Enter your power");
+      int p=sc.nextInt();
+      for(int i=1; i<=p;i++)
+      {
+         result=result*n;
+         System.out.println(result);
+      }
+   }    
+}
+//Write a java program to find power of any number x ^ y.
