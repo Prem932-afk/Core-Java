c41a32e33e9e67aee9a074e5fdd9478d3a855d37
prem singh <premshekhawat1997@gmail.com>
2025-03-08 15:30:43 +0530

    Write a java program to enter  P, T, R and calculate Simple Interest?

---
+++ Basic/Alltypes Q/Basics Program/Program17.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Program17 
+{
+  public static void main(String[] args) 
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number to calculate square root :");
+    double number=sc.nextDouble();
+    double squareroot=Math.sqrt(number);
+    if(number<0)
+    {
+        System.out.println("Negative number");
+    }
+    else
+    {
+    System.out.println("Your squarroot number :"+squareroot);
+    }
+    sc.close();
+  }    
+}
+// Write a java program to enter  P, T, R and calculate Simple Interest?
