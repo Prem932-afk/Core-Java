737b9bcb77d12d010a69f1a49c4e7eb32765047f
prem singh <premshekhawat1997@gmail.com>
2025-03-08 16:00:48 +0530

    1,2,3,4,5,6,7,8,9,10 : odd number print

---
+++ Basic/Alltypes Q/Arrays/Program11.java
@@ -0,0 +1,19 @@
+// Q.11
+
+// 1,2,3,4,5,6,7,8,9,10 :
+// odd number 
+
+public class Program11 
+{
+    public static void main(String[] args) 
+    {
+        int a[]={1,2,3,4,5,6,7,8,9,10};
+        for(int i=0; i<a.length; i++)
+        {
+            if(a[i]%2!=0)
+            {
+                System.out.println(a[i]);
+            }
+        } 
+    }
+}
