409afe435a6f2b31ec12bda8d84ed5ceaca29f1d
prem singh <premshekhawat1997@gmail.com>
2025-03-08 16:03:50 +0530

    output : 2,4,7,9

---
+++ Basic/Alltypes Q/Arrays/Program13.java
@@ -0,0 +1,23 @@
+Q.13
+ 
+1,2,3,4,5,6,7,8,9,10 :
+
+output :
+2,4,7,9
+
+public class Program13 {
+    public static void main(String[] args) 
+    {
+      int[] arr = {1,2,3,4,5,6,7,8,9,10 };
+          
+      
+      int[] secondArr = {2,4,7,9 };
+  
+     
+      System.out.print("Array: ");
+      for (int i = 0; i < secondArr.length; i++) 
+      {
+          System.out.print(secondArr[i] + " ");
+      }
+    }  
+}
