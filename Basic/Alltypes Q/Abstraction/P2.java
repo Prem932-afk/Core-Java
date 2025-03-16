dbd713fbe43621049a41061ec39611bea315347a
prem singh <premshekhawat1997@gmail.com>
2025-03-16 12:10:21 +0530

    Define an abstract class Animal with an abstract method makeSound(). Implement this in subclasses Dog, Cat, and Cow.

---
+++ Basic/Alltypes Q/Abstraction/P2.java
@@ -0,0 +1,52 @@
+
+// Q.2
+ 
+// Define an abstract class Animal with an abstract method makeSound(). Implement this in subclasses Dog, Cat, and Cow.
+
+
+
+
+public class P2 
+{
+   public static void main(String[] args) 
+   {
+      Cow c=new Cow();
+       c.makeSound();
+      Cat ca=new Cat();
+       ca.makeSound();
+      Dog d=new Dog();
+       d .makeSound();
+   }    
+}
+abstract class Animal
+{
+      abstract void makeSound(); 
+}
+
+class Dog extends Animal
+{
+     public void makeSound()
+     {
+        System.out.println("boo boo");
+     }
+}
+
+class Cat extends Animal
+{
+     public void makeSound()
+     {
+        System.out.println("Mew Mew");
+     }
+}
+
+class Cow extends Animal
+{
+     public void makeSound()
+     {
+        System.out.println("Aooo");
+     }
+}
+
+
+
+
