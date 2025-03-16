0e5498482318b956206e1b063b0c45f85aed1c36
prem singh <premshekhawat1997@gmail.com>
2025-03-16 12:05:50 +0530

    Create a Java program where a Dog class inherits a Mammal class, and call a method to display their characteristics.

---
+++ Basic/Alltypes Q/Inheritance/P2.java
@@ -0,0 +1,43 @@
+//2)
+//Create a Java program where a Dog class inherits a Mammal class, and call a method to display their characteristics.
+
+public class P2 
+{
+   public static void main(String[] args) 
+   {
+         Dog myDog = new Dog();
+
+        // Call the displayCharacteristics method
+        System.out.println("Dog Characteristics:");
+        myDog.displayCharacteristics();
+   } 
+}
+class mammal
+{
+    String species = "Mammal";
+    boolean hasHair = true;
+    boolean givesBirth = true;
+
+   
+     void displayCharacteristics() 
+    {
+        System.out.println("Species: " + species);
+        System.out.println("Has Hair: " + hasHair);
+        System.out.println("Gives Birth: " + givesBirth);
+    }
+}
+class Dog extends mammal
+{
+    String breed = "Golden Retriever";
+    String sound = "Bark";
+
+   
+    void displayCharacteristics() 
+    {
+        
+        super.displayCharacteristics();
+
+        System.out.println("Breed: " + breed);
+        System.out.println("Sound: " + sound);
+    }
+}
