da9272f4be9e0ac0b33b2f6ca616f1b7d09df2f2
prem singh <premshekhawat1997@gmail.com>
2025-03-16 11:55:29 +0530

    Create a class Book with instance variables title and author. Use a constructor to initialize these variables.Basic Constructor:

---
+++ Basic/Alltypes Q/Constructure/P1.java
@@ -0,0 +1,29 @@
+/* 
+Q.1)
+ 
+Basic Constructor:
+
+Create a class Book with instance variables title and author. Use a constructor to initialize these variables.
+*/
+
+class P1
+{
+   public static void main(String[] args) 
+   {
+      Book a=new Book("Java","james Gosling");  
+   }
+}
+
+class Book
+{
+      String title;
+      String Author;
+
+       Book( String title, String Author)
+      {
+          this.title=title;
+          this.Author=Author;
+          System.out.println("Title of Book :" + title);
+          System.out.println("Author of Book :" + Author);
+      }
+}
