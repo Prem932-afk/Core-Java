d04dfb16f2f12619c6ef40cf9d0119a89792641a
prem singh <premshekhawat1997@gmail.com>
2025-03-19 11:00:01 +0530

    2.Concat

---
+++ Basic/Alltypes Q/String/Concat.java
@@ -0,0 +1,10 @@
+public class Concat 
+{
+  public static void main(String[] args) 
+  {
+    String s1="prem";
+    String s2="Singh";
+    String s3=s1.concat(s2);
+    System.out.println(s3);
+  }    
+}
