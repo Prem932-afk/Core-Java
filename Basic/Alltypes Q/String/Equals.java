2d36dd802cf09a769501efa80037f7d7ea91a570
prem singh <premshekhawat1997@gmail.com>
2025-03-19 11:02:19 +0530

    3. Equals metho

---
+++ Basic/Alltypes Q/String/Equals.java
@@ -0,0 +1,9 @@
+public class Equals 
+{
+   public static void main(String[] args) 
+   {
+       String s1="Hello";
+       String s2="world";
+       System.out.println(s1.equals(s2));
+   }    
+}
