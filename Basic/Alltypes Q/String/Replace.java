a3958d0636dab755df12dcbc19b567f9fe6a2121
prem singh <premshekhawat1997@gmail.com>
2025-03-19 11:05:30 +0530

    8.Replace method

---
+++ Basic/Alltypes Q/String/Replace.java
@@ -0,0 +1,10 @@
+public class Replace 
+{
+  public static void main(String[] args) 
+  {
+       String s1="hellold";
+       String s2=s1.replace("old","world");
+       System.out.println(s2);
+
+  }    
+}
