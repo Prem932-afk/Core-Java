351e97b7fc266ef70e5480e905b09c755d3141be
prem singh <premshekhawat1997@gmail.com>
2025-03-19 11:07:14 +0530

    12.ValueOf method

---
+++ Basic/Alltypes Q/String/ValueOf.java
@@ -0,0 +1,14 @@
+public class ValueOf 
+{
+   public static void main(String[] args) 
+   {
+      int a=10;
+      int b=89;
+      System.out.println(a+b); 
+
+      String s1=String.valueOf(a);
+      String s2=String.valueOf(b);
+       System.out.println(s1+s2);
+            
+   }    
+}
