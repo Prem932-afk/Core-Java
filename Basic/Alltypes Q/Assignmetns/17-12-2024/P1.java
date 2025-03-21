a3920b7a585a063d7ddb58e5d700fa53e945e6ee
prem singh <premshekhawat1997@gmail.com>
2025-03-21 16:18:11 +0530

    Program 1

---
+++ Basic/Alltypes Q/Assignmetns/17-12-2024/P1.java
@@ -0,0 +1,18 @@
+// A :
+// 	method 1 :hello world
+class P1
+{
+    public static void main(String[] args)
+    {
+        A m=new A();
+        m.show();
+    }
+
+}
+class A
+{
+    void show()
+    {
+        System.out.println("Hello world");
+    }
+}
