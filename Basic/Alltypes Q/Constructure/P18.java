8ec8cfee3f480101bfae06a09684885bfdcc7f6e
prem singh <premshekhawat1997@gmail.com>
2025-03-24 16:46:26 +0530

    Define a class Light with a boolean instance variable isOn. Create a constructor that accepts a boolean value and initializes isOn.

---
+++ Basic/Alltypes Q/Constructure/P18.java
@@ -0,0 +1,31 @@
+// 18)
+// Constructor with Boolean Parameter:
+
+// Define a class Light with a boolean instance variable isOn. Create a constructor that accepts a boolean value and initializes isOn.
+
+
+public class P18 
+{
+    public static void main(String[] args) 
+    {
+
+        Light l =new Light(false);
+    }
+}
+class Light
+{
+    boolean isOn;
+    Light(boolean isOn)
+    {
+        this.isOn=isOn;
+        if(isOn==true)
+        {
+            System.out.println("Ligh is On : "+  isOn);
+        }
+        else
+        {
+           System.out.println("Light is Of : "+ isOn);
+        }
+        
+    }
+}
