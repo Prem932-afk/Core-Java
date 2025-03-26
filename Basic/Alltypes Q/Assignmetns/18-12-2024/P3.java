ae30b98210cb9dc4f0027dac2157f05da3a629c9
prem singh <premshekhawat1997@gmail.com>
2025-03-26 17:55:50 +0530

    Program 3

---
+++ Basic/Alltypes Q/Assignmetns/18-12-2024/P3.java
@@ -0,0 +1,42 @@
+ 
+// A :
+// primtiive data type
+
+
+
+
+public class P3 
+{
+public static void main(String[] args) 
+{
+      A a=new A();
+       a.show();
+       a.show2();
+       a.show3();
+}    
+}
+class A
+{
+   int show()
+   {
+     int n=3;
+     System.out.println("int : "+n);
+     return n;
+   }
+
+   char show2()
+   {
+      char ch='A';
+      System.out.println("Char :"+ ch);
+      return ch;
+   }
+
+   float show3()
+   {
+      float f= 1.2f;
+      System.out.println("Float : "+f);
+      return f;
+   }
+
+}
+
