607a86e1c8739047979e486cf3d9515f42dd0b69
prem singh <premshekhawat1997@gmail.com>
2025-03-30 13:23:55 +0530

    Program 7

---
+++ Basic/Alltypes Q/Assignmetns/20-12-2024/P7.java
@@ -0,0 +1,72 @@
+// //user input of instance variable through parameterised constructor and parameter name same : 
+
+// A :addition
+
+// B :sub
+
+// C :multi
+
+// D:div
+
+ 
+ 
+import java.util.*;
+public class P7
+    
+{
+   public static void main(String[] args) 
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter your number :");
+     int a=sc.nextInt();
+     int b=sc.nextInt();
+
+     A p=new A();
+      p.add(a,b);
+      p.sub(a,b);
+      p.mul(a,b);
+      p.div(a,b);
+
+      
+   }    
+}
+class A
+{
+    int a;
+    int b;
+
+    
+    void add(int a, int b)
+    {
+        this.a=a;
+        this.b=b;
+        int c=a+b;
+        System.out.println("Addition :"+c);
+    }
+
+    void sub(int a, int b)
+    {
+        this.a=a;
+        this.b=b;
+        int c=a-b;
+        System.out.println("Substracton :"+c);
+    }
+
+    void mul(int a, int b)
+    {
+          this.a=a;
+          this.b=b;
+          int c=a*b;
+          System.out.println("Multiple :"+c);
+    }
+
+    void div(int a, int b)
+    {
+          this.a=a;
+          this.b=b;
+          int c=a/b;
+          System.out.println("Divison :"+c);
+    }   
+
+}
+
