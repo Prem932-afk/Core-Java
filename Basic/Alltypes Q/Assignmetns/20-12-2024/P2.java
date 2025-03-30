10f30dfdbf108a8f69ec7450317b6eb27053165b
prem singh <premshekhawat1997@gmail.com>
2025-03-30 13:22:22 +0530

    Program 2

---
+++ Basic/Alltypes Q/Assignmetns/20-12-2024/P2.java
@@ -0,0 +1,50 @@
+
+
+// class A
+// 	constructor 
+// 	method :
+// 	no return and no argument 
+// 	return and argument 
+// 	return and no argument 
+
+// 	call two ways : 
+
+	
+  
+public class P2 
+{
+    public static void main(String[] args) 
+    {
+         
+         A a=new A();
+          a.show();
+          a.show1(2);
+          a.show2();
+    }
+}
+class A
+{ 
+    A()
+    {
+        System.out.println("Construcutre ");
+    }
+    
+    void show()
+    {
+        System.out.println("No return & No argument");
+    }
+
+    int show1(int n)
+    {
+        System.out.println("return & argument");
+        return n;
+    }
+
+    int show2()
+    {
+        int n=2;
+        System.out.println("return & No argument");
+        return n;
+    }
+}
+
