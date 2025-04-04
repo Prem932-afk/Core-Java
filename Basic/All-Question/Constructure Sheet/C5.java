08d8c3dad5925db406b93f523226f7aa0498ec4d
prem singh <premshekhawat1997@gmail.com>
2025-04-04 16:19:33 +0530

    Define a class Student with a no-argument constructor that initializes name to Anonymous and marks to 0.No-Argument Constructor:

---
+++ Basic/All-Question/Constructure Sheet/C5.java
@@ -0,0 +1,23 @@
+//Q.5
+
+// No-Argument Constructor:
+
+// Define a class Student with a no-argument constructor that initializes name to "Anonymous" and marks to 0.
+public class C5 
+{
+  public static void main(String[] args) 
+  {
+    new Student();
+    
+  }    
+}
+class Student
+{
+    Student()
+    {
+        String name="Anonymous";
+        int marks=300;
+        System.out.println("Name :"+name);
+        System.out.println("marks :"+marks);
+    }
+}
