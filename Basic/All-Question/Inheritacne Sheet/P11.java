6f121641a72739a46e39bd99f3a031ef93225de3
prem singh <premshekhawat1997@gmail.com>
2025-04-06 07:44:52 +0530

    Implement a program where a Building class is inherited by a House class, and then a Mansion class inherits from House. Each class should have unique properties.

---
+++ Basic/All-Question/Inheritacne Sheet/P11.java
@@ -0,0 +1,74 @@
+//11.Implement a program where a Building class is inherited by a House class, and then a Mansion class inherits from House. Each class should have unique properties. 
+
+
+
+class Building 
+{
+    String address;
+    int yearBuilt;
+
+   
+    void building(String address, int yearBuilt) 
+    {
+        this.address = address;
+        this.yearBuilt = yearBuilt;
+    }
+
+    
+     void displayDetails() 
+     {
+        System.out.println("Address: " + address);
+        System.out.println("Year Built: " + yearBuilt);
+        System.out.println("This is a general building.");
+    }
+
+   
+}
+
+
+class House extends Building 
+{
+    int numberOfRooms;
+
+  
+    void pouse(int numberOfRooms) 
+    {
+       
+        this.numberOfRooms = numberOfRooms;
+        System.out.println("Number of Rooms: " + numberOfRooms);
+        System.out.println("This is a residential house.");
+    }
+
+  
+        
+}
+
+
+class Mansion extends House 
+{
+    boolean hasSwimmingPool;
+
+
+    void mansion( boolean hasSwimmingPool) 
+    {
+       
+        this.hasSwimmingPool = hasSwimmingPool;
+        System.out.println("Has Swimming Pool: " + (hasSwimmingPool ? "Yes" : "No"));
+        System.out.println("This is a luxurious mansion with high-end features.");
+    
+    }
+
+}
+class Method31 
+{
+    public static void main(String[] args) 
+    {
+       Mansion m=new Mansion();
+       m.mansion(false);
+       m.pouse(5);
+       m.building("Rajasthan", 5);
+       m.displayDetails();
+    }
+}
+
+
