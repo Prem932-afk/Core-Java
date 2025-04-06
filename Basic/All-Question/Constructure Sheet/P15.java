2c0b7c9aa75a2076911b0784263472f2495421ef
prem singh <premshekhawat1997@gmail.com>
2025-04-06 07:17:05 +0530

    Create a class Circle with a radius instance variable. Use a constructor to initialize the radius value and then write a method to display the radius.

---
+++ Basic/All-Question/Constructure Sheet/P15.java
@@ -0,0 +1,27 @@
+
+// Constructor with Instance Variable Initialization:
+
+// Create a class Circle with a radius instance variable. Use a constructor to initialize the radius value and then write a method to display the radius.
+
+public class P15 
+{
+      public static void main(String[] args) 
+      {
+         Circle c = new Circle(33);
+         c.displayDetails();
+
+      }
+}
+class Circle 
+{
+    int radius;
+    Circle(int radius)
+    {
+          this.radius=radius;
+
+    }
+    void displayDetails()
+    {
+        System.out.println("Circle Radius : "+ radius);
+    }
+}
