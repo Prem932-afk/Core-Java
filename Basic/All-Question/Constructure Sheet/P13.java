695d57ded6aabc5420f2e3e8af260094175d4439
prem singh <premshekhawat1997@gmail.com>
2025-04-06 07:12:38 +0530

    Write a class Person with a constructor that initializes the name to John and age to 25. Create an object and display these values.

---
+++ Basic/All-Question/Constructure Sheet/P13.java
@@ -0,0 +1,21 @@
+// Constructor for Initialization:
+
+// Write a class Person with a constructor that initializes the name to "John" and age to 25. Create an object and display these values.
+
+public class P13 
+{
+     public static void main(String[] args) 
+     {
+             Person p =new Person();  
+     }
+}
+class Person
+{
+     Person()
+     {
+        String name="john";
+        int age=25;
+        System.out.println(" Person Name : "+name);
+        System.out.println("Person Age : "+age);
+     }  
+}
