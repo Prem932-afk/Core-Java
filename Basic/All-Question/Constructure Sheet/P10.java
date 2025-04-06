3be4f3898699be9af46822b36ee97c132ff64bcd
prem singh <premshekhawat1997@gmail.com>
2025-04-06 07:09:19 +0530

    Write a class Employee with an integer instance variable salary. Create a constructor that takes an integer and sets the salary value.

---
+++ Basic/All-Question/Constructure Sheet/P10.java
@@ -0,0 +1,23 @@
+
+
+public class P10 
+{
+  public static void main(String[] args) 
+  {
+    new Employee(4555); 
+  }    
+}
+class Employee
+{
+    int salary;
+    Employee(int salary)
+    {
+        this.salary=salary;
+        System.out.println("Employe salary :" +salary);
+    }
+}
+
+// Constructor with Integer Parameter:
+
+// Write a class Employee with an integer instance variable salary. Create a constructor that takes an integer and sets the salary value.
+
