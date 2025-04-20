cea83cbef39134d2528282ebda1fc1bb200882fd
prem singh <premshekhawat1997@gmail.com>
2025-04-20 14:46:56 +0530

    Program 1

---
+++ Basic/Core Java/Assignment/17-Dec-2024/A1.java
@@ -0,0 +1,22 @@
+//Q.1
+
+// A :
+// 	method 1 :hello world
+
+
+class A1
+{
+    public static void main(String[] args)
+    {
+        A m=new A();
+        m.show();
+    }
+
+}
+class A
+{
+    void show()
+    {
+        System.out.println("Hello world");
+    }
+}
