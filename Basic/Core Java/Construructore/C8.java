054b58163a71832fb31ed32ed6552a59ed54ac75
prem singh <premshekhawat1997@gmail.com>
2025-04-20 07:16:19 +0530

    8.Create a class Person with name and age as instance variables. Use a constructor to initialize these values, and then write a method displayInfo() that prints the name and age.

--- Basic/Core Java/Construructore/C8.java
+++ Basic/Core Java/Construructore/C8.java
@@ -30,35 +30,3 @@ class Person
           System.out.println("Person Age :"+age);
        }
 }
-//Constructor and Method:
-
-//Create a class Person with name and age as instance variables. Use a constructor to initialize these values, and then write a method displayInfo() that prints the name and age.
-
---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
- Q.9)
- 
- 
-Constructor with String Parameter:
-
-Define a class Book with a constructor that accepts a String representing the book’s title and assigns it to the title instance variable.
- 
- 
- 
-public class ConstrucotrSheet9 
-{
-   public static void main(String[] args) 
-   {
-      Book b=new Book("Java");
-   }    
-}
-class Book
-{
-     String title;
-     Book(String title)
-     {
-        this.title=title;
-        System.out.println("Book Title :"+title);
-     }
-}
