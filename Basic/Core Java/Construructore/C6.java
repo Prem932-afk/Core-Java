41036050ee0f0110d3f47bc7a4c35d82ddcc19f3
prem singh <premshekhawat1997@gmail.com>
2025-04-20 07:12:43 +0530

    Create a class Book with an instance variable title and write a constructor that accepts a title and assigns it to the instance variable.

---
+++ Basic/Core Java/Construructore/C6.java
@@ -0,0 +1,25 @@
+// Q.6)
+  
+// Constructor with One Parameter:
+
+// Create a class Book with an instance variable title and write a constructor that accepts a title and assigns it to the instance variable.
+
+
+
+class C6 
+{
+   public static void main(String[] args) 
+   {
+     new Book("Core-Java");
+   }    
+}
+class Book
+{
+     String title;
+     Book(String title)
+     {
+        this.title=title;
+        System.out.println("The Bokk title is :"+title);
+     }
+
+}
