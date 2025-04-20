e5c5a3552538a29df75d598efd570d3798f14615
prem singh <premshekhawat1997@gmail.com>
2025-04-20 07:10:37 +0530

    4.Create a class Animal with instance variables name and species. Use a constructor to initialize these variables.

---
+++ Basic/Core Java/Construructore/C4.java
@@ -0,0 +1,29 @@
+// Q.4)
+ 
+// Constructor Initialization:
+
+// Create a class Animal with instance variables name and species. Use a constructor to initialize these variables.
+
+
+
+
+
+public class C4 
+{
+   public static void main(String[] args) 
+   {
+       new Animal("Parrot", "Bird");
+   }    
+}
+class Animal
+{
+          String name;
+          String species;
+          Animal(String name, String species)
+          {
+              this.name=name;
+              this.species=species;
+              System.out.println("Name : "+name);
+              System.out.println("Species :"+ species);
+          }
+}
