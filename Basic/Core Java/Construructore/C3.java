a40a355d9b81c97fd5e17fd4a945d24d8ef06498
prem singh <premshekhawat1997@gmail.com>
2025-04-20 07:10:06 +0530

    Write a class Car that has a constructor to initialize the model and year of a car.

---
+++ Basic/Core Java/Construructore/C3.java
@@ -0,0 +1,23 @@
+// Q.3)
+ 
+// Constructor with Parameters:
+
+// Write a class Car that has a constructor to initialize the model and year of a car.
+
+
+public class C3 
+{
+ public static void main(String[] args) 
+ {
+     new Car("kia","2021");
+ }    
+}
+class Car
+{
+      Car(String model, String year)
+      {
+           System.out.println("Car Model :"+model);
+           System.out.println("Car Year :"+year);
+      }
+}
+
