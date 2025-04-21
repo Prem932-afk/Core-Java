42c15e3b3d84d5439bcb8bfcfe24e8b87fcbf64d
prem singh <premshekhawat1997@gmail.com>
2025-04-21 13:21:17 +0530

    11.  Create a Book class with title, author, and price as instance variables. Use a constructor to initialize these, and then write a method displayDetails() to print them.

---
+++ Basic/Core Java/Construructore/C11.java
@@ -0,0 +1,34 @@
+// Q.11)
+ 
+// Constructor and Display Method:
+
+// Create a Book class with title, author, and price as instance variables. Use a constructor to initialize these, and then write a method displayDetails() to print them.
+
+
+
+public class C11 
+{
+  public static void main(String[] args) 
+  {
+     Book b=new Book("java", "Jsmes Gosling", 999);
+     b.displayDetails();
+  }    
+}
+class Book
+{
+   String title;
+   String author;
+   double price;
+   Book(String title, String author, double price)
+   {
+        this.title=title;
+        this.author=author;
+        this.price=price;
+   }
+   void displayDetails()
+   {
+         System.out.println("Book title : "+ title);
+         System.out.println("Book author : "+author);
+         System.out.println("Book price : "+price);        
+   }
+}
