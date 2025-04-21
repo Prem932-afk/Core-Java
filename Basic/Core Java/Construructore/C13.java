933a9578be2c2b57a97777f6d91951306a60f84e
prem singh <premshekhawat1997@gmail.com>
2025-04-21 14:08:14 +0530

    14.Write a class Employee with name and salary as instance variables. Create a constructor that accepts both a String for name and an int for salary.

---
+++ Basic/Core Java/Construructore/C13.java
@@ -0,0 +1,33 @@
+// Q.13)
+  
+// Constructor for Initialization:
+
+// Write a class Person with a constructor that initializes the name to "John" and age to 25. Create an object and display these values.
+
+
+public class C13 
+{
+   public static void main(String args[]) 
+   {
+     Person p=new Person();
+     p.display();
+     
+   }    
+}
+class Person
+{
+    String name;
+    int age;
+    public Person()
+    {
+         name="John";
+         age=25;
+       
+    }
+    public void display()
+    {
+        System.out.println("Name :"+name);
+        System.out.println("Age : "+age);
+     
+    }
+}
---
+++ Basic/Core Java/Construructore/C14.java
@@ -0,0 +1,27 @@
+// Q.14)
+ 
+// Constructor with a String and an Integer:
+
+// Write a class Employee with name and salary as instance variables. Create a constructor that accepts both a String for name and an int for salary.
+
+
+public class C14
+{
+  public static void main(String[] args) 
+  {
+      Employee e=new Employee("Prem", 20000);
+  }    
+}
+class Employee
+{
+     String name;
+     double salary;
+
+     Employee(String name, double salary)
+     {
+         this.name=name;
+         this.salary=salary;
+         System.out.println("Name :"+name);
+         System.out.println("Salary :"+salary);
+     }
+}
