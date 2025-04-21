1ddb7fbf56d74c3d7e88f2c95896849819edadbe
prem singh <premshekhawat1997@gmail.com>
2025-04-21 14:25:26 +0530

    Program 4

---
+++ Basic/Core Java/Assignment/18-Dec-2024/A4.java
@@ -0,0 +1,40 @@
+ 
+// A class
+// string
+
+// B class
+// covariant return type
+// welcome to india
+
+
+
+import java.util.Scanner;
+class A4
+{
+public static void main(String[] args)
+{
+  A p=new A();
+  p.show();
+  B p1=new B();
+  p1.show();
+}
+}
+
+class A
+{
+String show()
+{
+    String name="Hello world";
+    System.out.println(name);
+    return name;
+}
+}
+
+class B
+{
+B show()
+{
+    System.out.println("Hello");
+    return this;
+}
+}
