65c64c0907b34d77482e70e497432d9a6d8e97fe
prem singh <premshekhawat1997@gmail.com>
2025-04-22 15:49:04 +0530

    Program 3

---
+++ Basic/Core Java/Assignment/20-Dec-2024/A3.java
@@ -0,0 +1,26 @@
+  
+// two number addition through instance variable way :constant 
+
+
+
+ 
+public class A3 
+{
+    
+    public static void main(String[] args) 
+    {
+        new A(2, 07);
+      
+    }
+}
+class A
+{
+    int a,b;
+    A(int a, int b)
+    {
+        this.a=a;
+        this.b=b;
+        int c=a+b;
+        System.out.println("Addition of two value : "+c);
+    }
+}
