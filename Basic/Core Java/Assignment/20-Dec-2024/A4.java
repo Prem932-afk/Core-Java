591f0726099d0673f49862b7693ff5d71c29eba6
prem singh <premshekhawat1997@gmail.com>
2025-04-22 15:51:35 +0530

    Program 4

---
+++ Basic/Core Java/Assignment/20-Dec-2024/A4.java
@@ -0,0 +1,60 @@
+ 
+// //user input of instance variable through method and parameter name is also different : 
+// add()
+// sub()
+// div()
+// multi() 
+// no third variable allow 
+
+
+
+ public class A4 
+{
+   public static void main(String[] args) 
+   {
+     A a=new A();
+      a.add(3, 4);
+      a.sub(4, 5);
+      a.mul(6, 7);
+      a.div(3,4);
+   }    
+}
+class A
+{
+    int a;
+    int b;
+
+    void add(int x,int y)
+    {
+          this.a=x;
+          this.b=y;
+          int c=a+b;
+          System.out.println("Addition :"+c);
+    }
+
+    void sub(int x, int y)
+    {
+        this.a=x;
+        this.b=y;
+        int c=a-b;
+        System.out.println("Substracton :"+c);
+    }
+
+    void mul(int x,int y)
+    {
+          this.a=x;
+          this.b=y;
+          int c=a*b;
+          System.out.println("Multiple :"+c);
+    }
+
+    void div(int x,int y)
+    {
+          this.a=x;
+          this.b=y;
+          int c=a/b;
+          System.out.println("Divison :"+c);
+    }   
+
+}
+
