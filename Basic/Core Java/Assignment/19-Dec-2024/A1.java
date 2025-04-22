1f95d9743fceb412fcd74d0a7937bd43374a4c58
prem singh <premshekhawat1997@gmail.com>
2025-04-22 12:55:06 +0530

    Program 1

---
+++ Basic/Core Java/Assignment/19-Dec-2024/A1.java
@@ -0,0 +1,37 @@
+
+ 
+// A :
+// primtiive data type 
+
+
+
+public class A1 
+{
+ public static void main(String[] args) 
+ {
+     A a=new A();
+      a.show(9);
+      a.show2('A');
+      a.show3(1.2f);  
+ }    
+}
+class A
+{
+  int show(int n)
+  {
+    System.out.println(n);
+    return n;
+  }
+
+  char show2(char ch)
+  {
+    System.out.println(ch);
+    return ch;
+  }
+
+  float show3(float f)
+  {
+    System.out.println(f);
+    return f;
+  }
+}
