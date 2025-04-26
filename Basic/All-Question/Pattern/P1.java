2be9d40c4b177bbae080ebb086a427317b4cebb3
prem singh <premshekhawat1997@gmail.com>
2025-04-26 19:47:37 +0530

    Pattern 3

---
+++ Basic/All-Question/Pattern/P1.java
@@ -0,0 +1,28 @@
+class P1
+{
+    public static void main(String[] args) 
+    {
+       char ch='A';
+       for(int i=0; i<=4; i++)
+       {
+        for(int j=0; j<=4; j++)
+        {
+            System.out.print(ch);
+            ch++;
+        }
+        System.out.println(" ");
+       }   
+    // int i=0;
+    // while(i<=4)
+    // {
+    //     int j=0;
+    //     while(j<=4)
+    //     {
+    //         System.out.print("*");
+    //         j++;
+    //     }
+    //     System.out.println(" ");
+    //     i++;
+    // }
+    }
+}
