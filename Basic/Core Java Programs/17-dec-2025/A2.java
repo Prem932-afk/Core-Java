9fc486cb5d3e830e189f7bfc0f7d65eecbd21f25
prem singh <premshekhawat1997@gmail.com>
2025-05-02 15:46:10 +0530

    Program 2

---
+++ Basic/Core Java Programs/17-dec-2025/A2.java
@@ -0,0 +1,26 @@
+//Q.2
+// A :
+// 	method1 :
+// 	method2 :
+
+class A2 
+{
+    public static void main(String[] args) 
+    {
+         A a= new A();
+         a.Method1();
+         a.Method2();
+    }
+}
+class A
+{
+    void Method1()
+    {
+        System.out.println("Method 1");
+    }
+
+    void Method2()
+    {
+        System.out.println("Method 2");
+    }
+}
