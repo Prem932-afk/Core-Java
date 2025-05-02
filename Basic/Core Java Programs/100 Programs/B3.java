dbe1125f5dada24022b05cc8539ced00e427cf89
prem singh <premshekhawat1997@gmail.com>
2025-05-02 15:56:45 +0530

    Q.3 Write a Java program to enter two numbers and perform all arithmetic operations ?

---
+++ Basic/Core Java Programs/100 Programs/B3.java
@@ -0,0 +1,59 @@
+//Q.3 Write a Java program to enter two numbers and perform all arithmetic operations ?
+
+
+import java.util.Scanner;
+    public class B3
+    {
+      public static void main(String[] args) 
+      {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter you choise :");
+        String s=sc.nextLine();
+       
+        if(s.equals("Adittion"))
+        {
+                   System.out.print("Enter first Number :");
+                   int a=sc.nextInt();
+                   System.out.print("Enter secon Number");
+                   int b=sc.nextInt();
+                   int add=a+b;
+                   System.out.println("Your value :"+add);
+        }
+
+        else if(s.equals("Substraction"))
+        {
+                   System.out.print("Enter first Number :");
+                   int a=sc.nextInt();
+                   System.out.print("Enter secon Number");
+                   int b=sc.nextInt();
+                   int sub=a-b;
+                   System.out.println("Your value :"+sub);
+        }
+
+        else if(s.equals("Multiple"))
+        {
+                   System.out.print("Enter first Number :");
+                   int a=sc.nextInt();
+                   System.out.print("Enter secon Number");
+                   int b=sc.nextInt();
+                   int mul=a*b;
+                   System.out.println("Your value :"+mul);
+        }
+
+        else if(s.equals("Divison"))
+        {
+                   System.out.print("Enter first Number :");
+                   int a=sc.nextInt();
+                   System.out.print("Enter secon Number");
+                   int b=sc.nextInt();
+                   int div=a/b;
+                   System.out.println("Your value :"+div);
+        }
+
+        else
+        {
+            System.out.print("Input not matched : sorry!");
+        }
+        sc.close();
+      }   
+    }
