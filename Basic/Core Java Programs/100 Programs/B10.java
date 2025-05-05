ee39e8b7b031331a3da0c6ff1a087db66749bfab
prem singh <premshekhawat1997@gmail.com>
2025-05-05 07:03:51 +0530

    10.Write a Java program to convert days into years, weeks and days.

---
+++ Basic/Core Java Programs/100 Programs/B10.java
@@ -0,0 +1,25 @@
+
+
+
+import java.util.Scanner;
+
+public class B10
+{
+   public static void main(String[] args) 
+   {
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter your day :");
+      int totaldays=sc.nextInt();
+
+      int year= totaldays/365;
+      int remainingDaysAfterYears = totaldays%365;
+      int weeks=remainingDaysAfterYears/7;
+      int days=remainingDaysAfterYears%7;
+     
+      System.out.println("Year :"+year);
+      System.out.println("remain day after year :"+remainingDaysAfterYears);
+      System.out.println("weeks :"+weeks);
+      System.out.println("days :"+days);
+   }    
+}
+// Write a Java program to convert days into years, weeks and days.
