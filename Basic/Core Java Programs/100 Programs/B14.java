683c215f99256b8378ecc66efccd94ff64919c73
prem singh <premshekhawat1997@gmail.com>
2025-05-06 08:01:32 +0530

    14. Write a java program to enter base and height of a triangle and find its area.

---
+++ Basic/Core Java Programs/100 Programs/B14.java
@@ -0,0 +1,32 @@
+
+//14. Write a java program to enter base and height of a triangle and find its area.
+//area = 0.5 * base * height;
+import java.util.Scanner;
+
+public class B14
+{
+    public static void main(String[] args) 
+    {
+       
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Enter the base of the triangle: ");
+        double base = scanner.nextDouble();
+
+  
+        System.out.print("Enter the height of the triangle: ");
+        double height = scanner.nextDouble();
+
+   
+        double area = 0.5 * base * height;
+
+       
+        System.out.println("The area of the triangle is: " + area);
+
+    
+        scanner.close();
+    }
+}
+
+
