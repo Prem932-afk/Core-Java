d13cf0a195d6ce5c891287100ff81fe4242a7a4d
prem singh <premshekhawat1997@gmail.com>
2025-05-06 07:58:50 +0530

    12. Write a java program to enter any number and calculate its square root ?

---
+++ Basic/Core Java Programs/100 Programs/B12.java
@@ -0,0 +1,20 @@
+
+//12. Write a java program to enter any number and calculate its square root ?
+
+
+import java.util.Scanner;
+public class B12 
+{
+  public static void main(String[] args) 
+  {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your number");
+        int n=sc.nextInt();
+        double m=Math.sqrt(n);
+        System.out.println(n+"  is  " +m);
+  }   
+}
+
+//Write a java program to enter any number and calculate its square root.
+
+
