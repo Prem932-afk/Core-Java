f1cb5ce46ae450fa42d7ec7f1ad0422a8673f9d1
prem singh <premshekhawat1997@gmail.com>
2025-05-11 08:05:54 +0530

    4. Write a Java program to enter length and breadth of a rectangle and find its perimeter ?

---
+++ Basic/100 Qustion Core java/P4.java
@@ -0,0 +1,27 @@
+//4. Write a Java program to enter length and breadth of a rectangle and find its perimeter ? 
+
+
+
+import java.util.Scanner;
+class P4 
+{
+ public static void main(String[] args)
+ {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter rectangle length :");
+    double length=sc.nextDouble();
+
+    System.out.println("Enter rectangle breath :");
+    double breath=sc.nextDouble();
+
+    double parameter=2* (length+breath);
+    System.out.println("Rectamge parameter : " + parameter);
+    sc.close();
+      
+ }   
+}
+
+
+//Perametier formula :
+  //                     parameter =2* (breadth *length)
+    
