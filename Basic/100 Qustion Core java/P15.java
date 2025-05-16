99ab5516845610f30baa24f93400b4bd0a335aa0
prem singh <premshekhawat1997@gmail.com>
2025-05-16 22:54:29 +0530

    Program 15

---
+++ Basic/100 Qustion Core java/P15.java
@@ -0,0 +1,22 @@
+//15. Write a java program to calculate area of an equilateral triangle ?
+
+
+import java.util.Scanner;
+public class P15 
+{
+  public static void main(String[] args) 
+  {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your height");
+        int height=sc.nextInt();
+
+        System.out.print("Enter your base");
+        int base=sc.nextInt();
+
+        int area=1/2*base*height;
+        System.out.println("Trainle area :"+area);
+  }    
+}
+//Write a java program to calculate area of an equilateral triangle.
+//area=1/2*base*height;
+
