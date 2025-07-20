7f0fbff5f4a3bc2c598df70f317b00bfadf3ed20
prem singh <premshekhawat1997@gmail.com>
2025-07-20 13:45:42 +0530

    Prgram 2

---
+++ Basic/Array Importene/P2.java
@@ -0,0 +1,22 @@
+//  2. Find the sum of all elements in an array :
+
+//  Explanation: Add all the elements of the array and return the total. Helps students understand loop traversal and addition.
+
+//  Input: arr = [5, 10, 15]
+//  Output: Sum: 30
+
+class P2
+{
+    public static void main(String[] args) 
+    {
+        int arr[]={5,10,15};
+        
+        int sum=0;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            sum=sum + arr[i];
+        }
+         System.out.println("Total Sum : " + sum);
+    }
+}
