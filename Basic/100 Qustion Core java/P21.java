5107045bfee6d172d67c088395e5645b2dd102ec
prem singh <premshekhawat1997@gmail.com>
2025-07-24 06:13:59 +0530

    Program 21

---
+++ Basic/100 Qustion Core java/P21.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class P21
+{
+    public static void main(String[] args) 
+    {
+       
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();  
+        
+      
+        if (number > 0) 
+        {
+            System.out.println("The number is positive.");
+        } 
+        else if (number < 0) 
+        {
+            System.out.println("The number is negative.");
+        } 
+        else 
+        {
+            System.out.println("The number is zero.");
+        }
+   
+        scanner.close();
+    }
+}
+//.Write a java program to check whether a number is negative, positive or zero.
