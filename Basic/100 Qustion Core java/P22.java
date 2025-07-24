d71a00c84e806961b56aa769425f310890363814
prem singh <premshekhawat1997@gmail.com>
2025-07-24 06:14:49 +0530

    Program 22

---
+++ Basic/100 Qustion Core java/P22.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class P22
+{
+    public static void main(String[] args) 
+	{
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 11 == 0) 
+		{
+            System.out.println("The number is divisible by both 5 and 11.");
+        } 
+		else 
+		{
+            System.out.println("The number is NOT divisible by both 5 and 11.");
+        }
+        scanner.close();
+    }
+}
+
+// Write a java program to check whether a number is divisible by 5 and 11 or not.
