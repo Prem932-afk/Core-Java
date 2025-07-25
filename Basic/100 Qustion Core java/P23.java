185dc0133679b7b0cd3dc7c3b09a9a489ff80ddf
prem singh <premshekhawat1997@gmail.com>
2025-07-25 06:52:08 +0530

    Program 23

---
+++ Basic/100 Qustion Core java/P23.java
@@ -0,0 +1,30 @@
+ 
+import java.util.Scanner;
+public class P23
+{
+    public static void main(String[] args) 
+    {
+       
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();  
+        
+       
+        if (number % 2 == 0) 
+        {
+            System.out.println("The number is even.");
+        } 
+        else 
+        {
+            System.out.println("The number is odd.");
+        }
+        
+        
+        scanner.close();
+    }
+}
+
+//Write a java program to check whether a number is even or odd. 
+
