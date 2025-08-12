f093009b6d3cbc59f96c289c5707e94106dc6d13
prem singh <premshekhawat1997@gmail.com>
2025-08-12 22:31:15 +0530

    Q.5  Write a java program to enter length and breadth of a rectangle and find its area.?

---
+++ Basic/J-Program/Basic 100/P5.java
@@ -0,0 +1,29 @@
+//Q.5  Write a java program to enter length and breadth of a rectangle and find its area.? 
+
+
+
+import java.util.Scanner;
+
+public class P5
+{
+    public static void main(String[] args) 
+    {
+        
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        
+        System.out.print("Enter the breadth of the rectangle: ");
+        double breadth = scanner.nextDouble();
+
+        double area = length * breadth;
+
+        
+        System.out.println("The area of the rectangle is: " + area);
+
+        scanner.close();
+    }
+}
