ceee09a80a5ac3170b2af610e580147f1aedcbc4
prem singh <premshekhawat1997@gmail.com>
2025-08-14 14:22:19 +0530

    Program 5

---
+++ Basic/J-Program/Array/Sheet-1/P5.java
@@ -0,0 +1,34 @@
+// 5. Reverse an array :
+
+// Explanation: Reverse the order of elements in the array. This demonstrates array manipulation and swapping.
+
+// Input: arr = [1, 2, 3, 4]
+// Output: [4, 3, 2, 1]
+
+public class P5 
+{
+    public static void main(String[] args) 
+    {
+         int arr[]={1,2,3,4,5};
+         
+         int i=0;                
+         int j=arr.length-1;   
+         int temp=0;        
+
+         while(i < j)   
+         {
+             temp=arr[i];    
+             arr[i]=arr[j];  
+             arr[j]=temp;   
+
+             i++;          
+             j--;           
+         }
+
+         System.out.println("Reversed Array");
+         for(int k=0; k<arr.length; k++)
+         {
+             System.out.println(arr[k]);
+         }
+    }
+}
