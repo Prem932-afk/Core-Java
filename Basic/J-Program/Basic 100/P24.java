e29e6f06527763c7140a651ca675d889c277f4cd
prem singh <premshekhawat1997@gmail.com>
2025-08-15 00:15:39 +0530

    Q.24 Write a java program to check year is leap year or not  ?

---
+++ Basic/J-Program/Basic 100/P24.java
@@ -0,0 +1,30 @@
+// Q.24 Write a java program to check year is leap year or not  ?
+
+import java.util.Scanner;
+public class P24
+{
+    public static void main(String[] args) 
+    {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();  
+        
+        
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+        {
+            System.out.println(year + " is a leap year.");
+        } 
+        else 
+        {
+            System.out.println(year + " is not a leap year.");
+        }
+        
+        
+        scanner.close();
+    }
+}
+
+//Write a java program to check whether a year is leap year or not.
