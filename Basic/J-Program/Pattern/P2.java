29e741d9af489b32650c2ec5ae47efa6470ffd94
prem singh <premshekhawat1997@gmail.com>
2025-08-15 00:10:39 +0530

    Pattern 2

---
+++ Basic/J-Program/Pattern/P2.java
@@ -0,0 +1,61 @@
+//* 
+//**
+//***
+//**** 
+//***** 
+
+class P2
+{
+    public static void main(String[] args) 
+    {
+        // 1. way
+
+        // for(int i=1; i<=5; i++)
+        // {
+        //     for(int j=1; j<=i; j++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+          // 2. way
+             
+        //   int i=0;
+        //   while(i<5)
+        //   {
+        //     int j=0;
+        //     while (j<=i)
+        //     {
+        //         System.out.print("*");
+        //         j++;
+        //     }
+        //     System.out.println(" ");
+        //     i++;
+        //   }
+
+        // 3. way
+
+        // for(int i=1; i<=5; i++)
+        // {
+        //     for(int j=1; j<=i; j++)
+        //     {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println(" ");
+        // }
+
+        // 4. way
+
+        // for(int i=1; i<=5; i++)
+        // {
+        //     for(int j=1; j<=i; j++)
+        //     {
+        //         System.out.print(i);
+        //     }
+        //     System.out.println(" ");
+        // }
+
+
+    } 
+}
