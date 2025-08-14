b41e4bc31bc1613d432ac6e3617524801ce31c4c
prem singh <premshekhawat1997@gmail.com>
2025-08-15 00:01:29 +0530

    Q.20 Write a java program to find Maximum three Number ?

---
+++ Basic/J-Program/Basic 100/P20.java
@@ -0,0 +1,35 @@
+// Q.20 Write a java program to find Maximum three Number ?
+
+
+
+import java.util.Scanner;
+
+public class P20
+{
+   public static void main(String[] args) 
+   {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter ther frist Number :");
+        int a=sc.nextInt();
+        System.out.println("Enter the Second Number :");
+        int b=sc.nextInt();
+        System.out.println("Enter the Third Number :");
+        int c=sc.nextInt();
+        if(a>b && a>c)
+        {
+            System.out.println("First Number is Maximum");
+        }
+        else if(b>a && b>c)
+        {
+            System.out.println("Second Number is Maxium");
+        }
+        else
+        {
+            System.out.println("Third Number is Maximum");
+        }
+        sc.close();
+   }    
+}
+//Write a java program to find Maximum three Number ?
+
+
