c48ae45f434cc7bbcb06996b4da13fc316e15678
prem singh <premshekhawat1997@gmail.com>
2025-08-15 14:43:47 +0530

    20.  Create a class Book with title and author as instance variables. In the main method, create an object using the constructor and display the title and author.

---
+++ Basic/J-Program/Construte Sheet/Book.java
@@ -0,0 +1,31 @@
+// Q.19)
+ 
+// Constructor in Main Method:
+
+// Create a class Book with title and author as instance variables. In the main method, create an object using the constructor and display the title and author.
+
+
+
+public class Book
+{
+ 
+    String title;
+    String author;
+
+     Book(String title, String author) 
+    {
+        this.title = title;
+        this.author = author;
+    }
+
+    
+    public static void main(String[] args) 
+    {
+       
+        Book myBook = new Book("The Great Gatsby", "F. Scott Fitzgerald");
+
+       
+        System.out.println("Title: " + myBook.title);
+        System.out.println("Author: " + myBook.author);
+    }
+}
