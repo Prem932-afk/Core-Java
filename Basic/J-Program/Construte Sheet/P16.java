6f7e653bca83ca48264e3259afb40bcb28b346f3
prem singh <premshekhawat1997@gmail.com>
2025-08-15 14:40:40 +0530

    16. Define a class Car that has model and year as instance variables. Write a constructor that accepts model and sets year to 2024 by default.

---
+++ Basic/J-Program/Construte Sheet/P16.java
@@ -0,0 +1,28 @@
+
+//  Q.16)
+ 
+// Constructor with Parameter and Default Initialization:
+
+// Define a class Car that has model and year as instance variables. Write a constructor that accepts model and sets year to 2024 by default.
+
+public class P16
+{
+    public static void main(String[] args) 
+    {
+        new Car();
+    }
+}
+
+class Car 
+{
+    String model;
+    String year;
+
+    Car() 
+    {
+        model = "2024";
+        year = "default";
+        System.out.println("Model :" + model);
+        System.out.println("Year :" + year);
+    }
+}
