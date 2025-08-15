64700a6a46e6ed09f000ce5c961ee85c99aa417a
prem singh <premshekhawat1997@gmail.com>
2025-08-15 05:57:20 +0530

    6. Create a class Book with an instance variable title and write a constructor that accepts a title and assigns it to the instance variable

---
+++ Basic/J-Program/Construte Sheet/P5.java
@@ -0,0 +1,25 @@
+//  Q.5)
+ 
+// No-Argument Constructor:
+
+// Define a class Student with a no-argument constructor that initializes name to "Anonymous" and marks to 0.
+
+
+
+public class P5
+{
+  public static void main(String[] args) 
+  {
+     new Student();
+  }    
+}
+class Student
+{
+          Student()
+          {
+             String name="Anonymous";
+             int marks=20;
+             System.out.println("Name :"+name);
+             System.out.println("Marks :"+marks);
+          }
+}
