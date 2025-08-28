36b38cf226c599d51c13c58a9641e232ec817e84
prem singh <premshekhawat1997@gmail.com>
2025-08-28 20:03:45 +0530

    Block

---
+++ Basic/Practise Questions/OOPs/P3.java
@@ -0,0 +1,27 @@
+//OOPs Block
+
+public class P3 
+{
+    // Constructure
+    P3()
+    {
+        System.out.println("Constructure ");
+    }
+  
+    // Inzilization block
+    
+    {
+        System.out.println("Iniazilatization block");
+    }
+
+    // static Inzilizatio block
+
+    static 
+    {
+        System.out.println("Static Inzilization block");
+    }
+   public static void main(String[] args) 
+   {
+        P3 p=new P3();
+   }    
+}
