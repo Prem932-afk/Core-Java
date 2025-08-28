9dc6e6e0b903eb0ec0dc425b27f71ded5f988b12
prem singh <premshekhawat1997@gmail.com>
2025-08-28 20:20:14 +0530

    No return & No Argument

---
+++ Basic/Practise Questions/Method/P1.java
@@ -0,0 +1,17 @@
+//1. No return & No argument
+class P1
+{ 
+   void shoe()
+   {
+    System.out.println("No return & No argument");
+   }
+}
+
+class Demo
+{
+    public static void main(String[] args) 
+    {
+       P1 p=new P1();
+         p.shoe();    
+    }
+}
