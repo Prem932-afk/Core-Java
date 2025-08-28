5d5f63db7fb1ae915ab45e82a7f0580f03762cf1
prem singh <premshekhawat1997@gmail.com>
2025-08-28 19:04:29 +0530

    Program 2

---
+++ Basic/Practise Questions/String/P2.java
@@ -0,0 +1,18 @@
+public class P2 
+{
+  public static void main(String[] args) 
+  {
+      char ch[]={'a','b','c','d'};
+      
+      String s=new String(ch);
+
+      System.out.println("Char Array :" + s);
+
+
+      byte b[]={101,102,103,104,107};
+
+      String s1=new String(b);
+
+      System.out.println("Byte Array :" + s1);
+  }    
+}
