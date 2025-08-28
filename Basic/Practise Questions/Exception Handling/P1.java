9e50a027b6ba9c9b772fcb637ac25f067e767d9b
prem singh <premshekhawat1997@gmail.com>
2025-08-28 19:37:58 +0530

    Exception Handling method

---
+++ Basic/Practise Questions/Exception Handling/P1.java
@@ -0,0 +1,20 @@
+class P1
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            int a=100;
+            int b=0;
+            int c = a/b;
+        }
+        catch(Exception e)
+        {
+            //e.printStackTrace();
+           // System.out.println(e);
+
+           e.getMessage();
+        }
+
+    }
+}
