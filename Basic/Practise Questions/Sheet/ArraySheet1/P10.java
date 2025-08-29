475bcbc67283577f9bdf7a9aa6a254c46991bfbf
prem singh <premshekhawat1997@gmail.com>
2025-08-29 22:31:02 +0530

    Program 10

---
+++ Basic/Practise Questions/Sheet/ArraySheet1/P10.java
@@ -0,0 +1,26 @@
+// Q.10   Copy elements from one array to another :
+// Explanation: Copy all elements of one array into another. Helps students understand array initialization and traversal.
+
+//  Input:  arr1 = [1, 2, 3]
+//  Output: arr2 = [1, 2, 3]
+public class P10 
+{
+  public static void main(String[] args) 
+  {
+       int arr1[]={1, 2, 3};
+       int arr2[]=new int[3];
+       
+       for(int i=0; i<arr1.length; i++)
+       {
+          System.out.println(arr1[i]);
+       }
+
+       System.out.println("Print Second Array");
+
+       for(int i=0; i<arr1.length; i++)
+       {
+         arr2[i]=arr1[i];
+         System.out.println(arr2[i]);
+       }
+  }    
+}
