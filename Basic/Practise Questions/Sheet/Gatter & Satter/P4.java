899bf192d8411d3a61ee262a579a55f5127058a8
prem singh <premshekhawat1997@gmail.com>
2025-08-31 14:57:05 +0530

    Pogram 4

---
+++ Basic/Practise Questions/Sheet/Gatter & Satter/P4.java
@@ -0,0 +1,39 @@
+//Q.4)
+ 
+//Validation in Setter.
+
+//In the Employee class, create a setter for the salary field (double). Ensure that the salary cannot be set to a negative value. If the value is invalid, set it to 0.
+// ployee class, create a setter for the salary field (double). Ensure that the salary cannot be set to a negative value. If the value is invalid, set it to 0.
+
+
+class P4
+{
+    public static void main(String[] args) 
+    {
+         Employee e=new Employee();
+           e.setSlary(30000);
+           System.out.println(e.getSalary());
+    }
+}
+
+class Employee
+{
+ private double salary;
+
+ void setSlary(double salary)
+ {
+       if(salary<0)
+       {
+          this.salary=0;
+       }
+       else
+       {
+          this.salary=salary;
+       }
+ }
+
+ double getSalary()
+ {
+    return salary;
+ }
+}
