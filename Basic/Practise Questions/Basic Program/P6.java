97254c2589c34415fa7ac9058858fd06fb1535ba
prem singh <premshekhawat1997@gmail.com>
2025-09-01 14:13:09 +0530

    6.Print even number?

---
+++ Basic/Practise Questions/Basic Program/P6.java
@@ -0,0 +1,12 @@
+//5.Print even number?
+public class P6
+{
+  public static void main(String[] args) 
+  {
+     int n=50;
+     for(int i=0; i<=n; i=i+2)
+     {
+        System.out.println(i);
+     }
+  }    
+}
