19a0b7c0628cebffc45344ff13901030f41c651d
prem singh <premshekhawat1997@gmail.com>
2025-09-01 14:09:29 +0530

    4. Sum of n natural number?

---
+++ Basic/Practise Questions/Basic Program/P4.java
@@ -0,0 +1,16 @@
+//4. Sum of n natural number?
+public class P4 
+{
+  public static void main(String[] args) 
+  {
+      int n=10;
+      int sum=0;
+      for(int i=1; i<=n; i++)
+      {
+        sum=sum + i;
+        
+      } 
+      System.out.println("Sum of N Natural Number :" + sum);
+
+  }    
+}
