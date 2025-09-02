2a714cd4fd91408f566ac9f89e441b73c13d0bb3
prem singh <premshekhawat1997@gmail.com>
2025-09-03 00:50:33 +0530

    Q.13 Write a java program to enter two angles of a triangle and find the third angle ?

---
+++ Basic/Practise Questions/100 Program/P13.java
@@ -0,0 +1,32 @@
+// Q.13 Write a java program to enter two angles of a triangle and find the third angle ?
+
+
+import java.util.*;
+public class P13
+{
+  public static void main(String[] args) 
+  {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter trinagl first angle :");
+        double firstangle=sc.nextDouble();
+        
+        System.out.print("Enter trianlge second angle :");
+        double secondangle =sc.nextDouble();
+        double thirdAngle= 180-(firstangle + secondangle);
+		
+        if(firstangle<=0 || secondangle<=0)
+        {
+            System.out.println("Error: Angles must be positive numbers.");
+        }
+        else if(firstangle + secondangle >= 180)
+        {
+            System.out.println("Error: The sum of the two angles must be less than 180 degrees");
+        }
+        else
+        {
+            System.out.println("The third angle of the triangle is: \" + thirdAngle + \" degrees.");
+        }
+  }    
+}
+//Write a java program to enter two angles of a triangle and find the third angle.
+
