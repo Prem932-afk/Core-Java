fc47b56abacae1b042112be1e083e79185528d5b
prem singh <premshekhawat1997@gmail.com>
2025-09-02 14:58:08 +0530

    25. Write a java program to check whether a character is alphabet or not.

---
+++ Basic/Practise Questions/100 Program/P25.java
@@ -0,0 +1,31 @@
+// Q.25 Write a java program to check whether a character is alphabet or not ?
+ 
+import java.util.Scanner;
+
+public class P25
+{
+    public static void main(String[] args) 
+    {
+   
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0);  
+        
+       
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) 
+        {
+            System.out.println(ch + " is an alphabet.");
+        } 
+        else 
+        {
+            System.out.println(ch + " is not an alphabet.");
+        }
+        
+   
+        scanner.close();
+    }
+}
+
+//Write a java program to check whether a character is alphabet or not.
