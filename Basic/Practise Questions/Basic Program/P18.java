fd0a341589c04cb736e299ac042df57bf8333187
prem singh <premshekhawat1997@gmail.com>
2025-09-02 15:21:13 +0530

    18. Pirnt table

---
+++ Basic/Practise Questions/Basic Program/P18.java
@@ -0,0 +1,17 @@
+//18. Table print
+import java.util.*;
+public class P18 
+{
+ public static void main(String[] args) 
+ {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter your number");
+      int n=sc.nextInt();
+      
+      for(int i=1; i<=10; i++)
+      {
+      
+        System.out.println(n*i);
+      }
+ }    
+}
