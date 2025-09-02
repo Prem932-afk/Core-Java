14662ed4693e15b2e7cb69da4d63f38558aa100b
prem singh <premshekhawat1997@gmail.com>
2025-09-02 15:07:33 +0530

    14. Aberage number

---
+++ Basic/Practise Questions/Basic Program/P14.java
@@ -0,0 +1,16 @@
+//Q.14 Average total marks?
+public class P14 
+{
+  public static void main(String[] args) 
+  {
+     int maths=65;
+     int eng=77;
+     int hindi=56;
+     int science=44;
+     int total = maths + eng + hindi + science;
+     System.out.println("Total Marks :" + total);
+     
+     int Average= total / 4;
+     System.out.println("Average marks :" + Average);
+  }    
+}
