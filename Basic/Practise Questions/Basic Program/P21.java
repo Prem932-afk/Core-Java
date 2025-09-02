65ac49642f8c129b06f38f7dfdfbac4377d7f0aa
prem singh <premshekhawat1997@gmail.com>
2025-09-03 01:12:24 +0530

    21. Clacluate Rectagnel area

---
+++ Basic/Practise Questions/Basic Program/P21.java
@@ -0,0 +1,17 @@
+//21.Rectangle area
+
+import java.util.*;
+public class P21 
+{
+ public static void main(String[] args) 
+ {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Rectange lenght");
+    int lenght=sc.nextInt();
+    System.out.println("Enter Rectangle bread");
+    int bread=sc.nextInt();
+
+    int area=lenght * bread;
+    System.out.println(area);
+ }    
+}
