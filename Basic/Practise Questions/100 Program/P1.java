1d54449cd6aeb4f22cef817d6b786a8e68129a98
prem singh <premshekhawat1997@gmail.com>
2025-09-02 06:33:29 +0530

    1.  Write a Java program to perform input/output of all basic data types ?

---
+++ Basic/Practise Questions/100 Program/P1.java
@@ -0,0 +1,32 @@
+//1.  Write a Java program to perform input/output of all basic data types ?
+import java.util.Scanner;
+class P1
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Interger Number");
+        int n=sc.nextInt();
+        System.out.println("Integer Number :"+n); 
+        System.out.println(" ");
+
+        System.out.println("Enter Float Number");
+        float f=sc.nextFloat();
+        System.out.println("Float Number :"+f); 
+        System.out.println(" ");
+
+        System.out.println("Enter Double Number");
+        Double d=sc.nextDouble();
+        System.out.println("Double Number :"+d); 
+        System.out.println(" ");
+
+        System.out.println("Enter Charector ");
+        char ch=sc.next().charAt(0);
+        System.out.println("Charector Value : "+ch); 
+        System.out.println(" ");
+
+        System.out.println("Enter String name");
+        String s=sc.nextLine();
+        System.out.println("String Datatype :"+s); 
+    }
+}
