c7eb05ddc058841c3f7f041e450f0d5589fa189c
prem singh <premshekhawat1997@gmail.com>
2025-09-03 10:55:36 +0530

    Program 41

---
+++ Basic/Practise Questions/100 Program/P41.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class P41
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int remainder = num % 2;
+
+        switch (remainder) 
+        {
+            case 0:
+                System.out.println("Even");
+                break;
+            case 1:
+                System.out.println("Odd");
+                break;
+        }
+    }
+}
