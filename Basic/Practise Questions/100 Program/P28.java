1dc1053565d8589b9deac13e8cc7510560510010
prem singh <premshekhawat1997@gmail.com>
2025-09-03 10:53:32 +0530

    Program 28

---
+++ Basic/Practise Questions/100 Program/P28.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class P28
+{
+    public static void main(String[] args) 
+    {
+        // Create a Scanner object for taking input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt the user to enter a character
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0);
+        
+        // Check if the character is an uppercase alphabet
+        if (Character.isUpperCase(ch))// (ch=='A' && ch=='Z')
+         {
+            System.out.println(ch + " is an uppercase alphabet.");
+         }
+        // Check if the character is a lowercase alphabet
+        else if (Character.isLowerCase(ch)) //(ch=='a' && ch=='z')
+        {
+            System.out.println(ch + " is a lowercase alphabet.");
+        } 
+        else 
+        {
+            System.out.println(ch + " is not an alphabet.");
+        }
+        
+        // Close the scanner
+        scanner.close();
+    }
+}
+//.Write a C program to check whether a character is uppercase or lowercase alphabet.
