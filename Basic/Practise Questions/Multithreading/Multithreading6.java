0b48850b3d034a590128621a4ed29971df039baa
prem singh <premshekhawat1997@gmail.com>
2025-09-05 04:51:33 +0530

    1.sleep

---
+++ Basic/Practise Questions/Multithreading/Multithreading6.java
@@ -0,0 +1,18 @@
+public class Multithreading6 
+{
+ public static void main(String[] args) 
+ {
+    for(int i=1; i<=5; i++)
+    {
+        try
+        {
+           Thread.sleep(1000);
+           System.out.println(i);
+        }
+        catch(Exception e)
+        {
+          System.out.println(e);
+        }
+    }   
+ }    
+}
