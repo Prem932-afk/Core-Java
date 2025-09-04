552090bc188cfdf3795281dd1d4dc43ea265f984
prem singh <premshekhawat1997@gmail.com>
2025-09-04 10:56:08 +0530

    1. tOString object

---
+++ Basic/Practise Questions/Lang pacakge/Student.java
@@ -0,0 +1,23 @@
+class Student
+{
+    private int age;
+    private String name;
+
+    Student(int age, String name)
+    {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Student[age=" + age + ", name=" + name + "]";
+    }
+
+    public static void main(String args[])
+    {
+        Student s = new Student(10, "Rahul");
+        System.out.println(s);
+    }
+}
