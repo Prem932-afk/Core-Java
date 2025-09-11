33308e2381495129650ea9d08eaa6ebcdf5ac510
prem singh <premshekhawat1997@gmail.com>
2025-09-11 15:03:29 +0530

    Program 4

---
+++ Basic/All-Question/Basic Program/P4.java
@@ -0,0 +1,27 @@
+//1. Bubble Sort
+public class P4 
+{
+    public static void main(String[] args) 
+    {
+        int arr[] = {10, 25, 12, 40, 6};
+        int temp = 0;
+        int swapcount=0;
+
+        for (int i = 0; i < arr.length - 1; i++) 
+        {
+            for (int j = 0; j < arr.length - i - 1; j++) 
+            {
+                if (arr[j] > arr[j + 1]) 
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1]; 
+                    arr[j + 1] = temp;
+                    swapcount++;
+                }
+            }
+        }
+
+        System.out.println("Total SwapCount :" + swapcount);
+      
+    }    
+}
