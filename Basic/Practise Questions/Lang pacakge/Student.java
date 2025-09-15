8399ece3f0b0fbe23be47db9139c0805a3c758de
prem singh <premshekhawat1997@gmail.com>
2025-09-15 13:59:05 +0530

    toString method

--- Basic/Practise Questions/Lang pacakge/Student.java
+++ Basic/Practise Questions/Lang pacakge/Student.java
@@ -1,23 +1,23 @@
-class Student
+public class Student
 {
     private int age;
     private String name;
 
     Student(int age, String name)
     {
-        this.age = age;
-        this.name = name;
+        this.age=age;
+        this.name=name;
     }
 
     @Override
     public String toString()
     {
-        return "Student[age=" + age + ", name=" + name + "]";
+        return "Student[age" + age + "name" + name + "]";
     }
 
-    public static void main(String args[])
-    {
-        Student s = new Student(10, "Rahul");
-        System.out.println(s);
-    }
+   public static void main(String[] args) 
+   {
+       Student s=new Student(10, "Rahul");
+       System.out.println(s);
+   }    
 }
