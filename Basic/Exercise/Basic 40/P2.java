ec6790ab40de32d8090dc0a20d8751a8d76a91ec
prem singh <premshekhawat1997@gmail.com>
2025-09-18 00:09:53 +0530

    2. Write a Java program to N natural Number ?

---
+++ Basic/Exercise/Basic 40/P2.java
@@ -0,0 +1,17 @@
+//3. Write a Java program to N natural Number ?
+
+import java.util.*;
+public class P2 
+{
+   public static void main(String[] args) 
+   {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter your number");
+      int n=sc.nextInt();
+      
+      for(int i=1; i<=n; i++)
+      {
+        System.out.println(i);
+      }
+   }    
+}
