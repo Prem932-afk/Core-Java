62da3305673c2d1b173149b152c7b04b8a3a2b7c
prem singh <premshekhawat1997@gmail.com>
2025-09-18 12:24:57 +0530

    1. Collection Example

---
+++ Basic/Java Program(CJ)/Collections/P1.java
@@ -0,0 +1,15 @@
+//1. Collection Example
+
+import java.util.*;
+class P1
+{
+    public static void main(String[] args) 
+    {
+         Collection<String> frutis=new ArrayList();
+           frutis.add("Apple");
+           frutis.add("Banana");
+           frutis.add("Mango");
+
+         System.out.println(frutis);  
+    }
+}
