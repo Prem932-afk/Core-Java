c8ad70958d6dc9102981743c3329f65544eb2453
prem singh <premshekhawat1997@gmail.com>
2025-09-19 12:29:26 +0530

    Program 5

---
+++ Basic/Java Program(CJ)/Sheet/Mehtod Sheet/P5.java
@@ -0,0 +1,23 @@
+// 5. Sum of cubes of digits.  
+// Input: 153  
+// Output: 153  
+// Explanation: 1³+5³+3³=153
+
+class P5 
+{
+    public static void main(String[] args) 
+    {
+        int num = 153;
+        int sum = 0;
+        int temp = num;
+
+        while (temp > 0) 
+        {
+            int digit = temp % 10;
+            sum = sum + (digit * digit * digit);
+            temp = temp / 10;
+        }
+
+        System.out.println(sum);  // Output: 153
+    }
+}
