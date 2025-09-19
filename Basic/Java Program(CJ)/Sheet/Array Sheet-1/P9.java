4e9067decd878c2f79b2ca48282813eb9c1d5373
prem singh <premshekhawat1997@gmail.com>
2025-09-19 12:24:43 +0530

    Program 9

---
+++ Basic/Java Program(CJ)/Sheet/Array Sheet-1/P9.java
@@ -0,0 +1,21 @@
+// 9. Find the index of a specific element :
+
+// Explanation: Search for a specific number in the array and return its index. If not found, return -1.
+
+// Input: arr = [1, 3, 5, 7], key = 5
+// Output: Index: 2
+public class P9 
+{
+    public static void main(String[] args) 
+    {
+           int arr[]={1,3,5,7}; 
+           int key=5;
+          for(int i=0; i<arr.length; i++)
+          {
+                if(key==arr[i])
+                {
+                    System.out.println(i);
+                }
+          } 
+    }    
+}
