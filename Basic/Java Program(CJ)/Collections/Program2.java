2256e0b5319382d8cf6e162114127f46d22652cd
prem singh <premshekhawat1997@gmail.com>
2025-09-21 18:37:17 +0530

    2.Utility class

---
+++ Basic/Java Program(CJ)/Collections/Program2.java
@@ -0,0 +1,24 @@
+//2.Utility Class
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
+
+public class Program2 
+{
+  public static void main(String[] args) 
+  {
+    List<Integer> l=new ArrayList<>();
+     
+      l.add(10);
+      l.add(20);
+      l.add(2);
+      l.add(1);
+
+      Collections.sort(l);
+
+      System.out.println(l);
+
+
+  }    
+}
