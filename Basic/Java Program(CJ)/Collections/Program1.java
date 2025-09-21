1b2dba08dfacce4bbdd217e454ddb15e5c516e85
prem singh <premshekhawat1997@gmail.com>
2025-09-21 18:31:40 +0530

    1.Program : Collection

---
+++ Basic/Java Program(CJ)/Collections/Program1.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Program1 
+{
+  public static void main(String args[])
+  {
+      Collection<String> fruits=new ArrayList<>();
+        fruits.add("Mango");
+        fruits.add("Banana");
+        fruits.add("Apple");
+
+        System.out.println(fruits);
+  }    
+}
