694f6fdf065ddc2f3921bfa6f3455074e5f91391
prem singh <premshekhawat1997@gmail.com>
2025-09-21 18:40:46 +0530

    Program 3

---
+++ Basic/Java Program(CJ)/Collections/Program3.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class Program3 
+{
+  public static void main(String[] args) 
+  {
+     ArrayList<Object> al=new ArrayList<>();
+       al.add("null");
+       al.add("null");
+       al.add(10);
+       al.add(10);
+       al.add(20);
+
+       System.out.println(al);
+      
+        System.out.println(  al.contains(10));
+  }    
+}
