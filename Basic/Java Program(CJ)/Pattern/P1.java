fe09243cc31589611e16fd5a86addc0616df6100
prem singh <premshekhawat1997@gmail.com>
2025-09-24 07:10:50 +0530

    Patter 1

---
+++ Basic/Java Program(CJ)/Pattern/P1.java
@@ -0,0 +1,30 @@
+class P1
+{
+    public static void main(String[] args) 
+    {
+        // for(int i=1; i<=5; i++)
+        // {
+        //     for(int j=1; j<=5; j++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //    System.out.println(" "); 
+        // }   
+
+        //.................While..............
+
+        int i=1;
+        while(i<=5)
+        {
+            int j=1; 
+            while(j<=5)
+            {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println(" ");
+            i++;
+        }
+         
+    }
+}
