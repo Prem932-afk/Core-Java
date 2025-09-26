1294671946b5bafeb223b7b75ea64ba4cc080c04
prem singh <premshekhawat1997@gmail.com>
2025-09-26 23:08:48 +0530

    2. Collections Example

---
+++ Basic/All-Question/New folder/Collections/Program2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+public class Program2 
+{
+  public static void main(String[] args) 
+  {
+        List<Integer> Number=new ArrayList<>();
+         Number.add(10);
+         Number.add(30);
+         Number.add(14);
+
+         Collections.sort(Number);
+
+        System.out.println(Number);
+  }    
+}
