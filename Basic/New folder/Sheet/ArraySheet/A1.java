28837e4789afa56a43c33c8e42db7e6055baaa7c
prem singh <premshekhawat1997@gmail.com>
2025-09-27 23:08:23 +0530

    1.Print the arry

---
+++ Basic/New folder/Sheet/ArraySheet/A1.java
@@ -0,0 +1,19 @@
+//Q.1  Print the elements of an array :
+// Explanation: Simply traverse the array and print each element. This teaches students 
+// how to iterate through an   ar    using loops.
+
+//   Input: arr = [1, 2, 3, 4, 5]
+//   Output: 1 2 3 4 5
+
+public class A1 
+{
+  public static void main(String[] args) 
+  {
+      int arr[]={1, 2, 3, 4, 5};
+
+      for(int i=0; i<=arr.length; i++)
+      {
+        System.out.println(arr[i]);
+      }
+  }    
+}
