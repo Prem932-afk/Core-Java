71fa4797cd9d1445b020bbe20d0422bd9e9b34cf
prem singh <premshekhawat1997@gmail.com>
2025-09-27 23:18:51 +0530

    Program 3

---
+++ Basic/New folder/Sheet/ArraySheet/A3.java
@@ -0,0 +1,25 @@
+// Q.3  Find the largest element in an array :
+
+//  Explanation: Compare each element of the array to track the maximum value. Teaches comparison logic.
+
+//   Input: arr = [10, 20, 5, 8]
+//   Output: Largest: 20
+
+public class A3 
+{
+    public static void main(String[] args) 
+    {
+       int arr[]={10, 20, 5, 8};
+       int largest=0;
+
+       for(int i=0; i<arr.length; i++)
+       {
+             if(arr[i]>largest)
+             {
+                largest=arr[i];
+             }
+       }
+
+       System.out.println("Your Largest Elements : " + largest);
+    }
+}
