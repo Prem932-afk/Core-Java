f945a410dc5d82fee3229f42362370d33071184a
prem singh <premshekhawat1997@gmail.com>
2025-10-05 23:14:49 +0530

    2. Add Two number

---
+++ Basic/Prem/Basic40/P2.java
@@ -0,0 +1,18 @@
+
+//2. Add Two number ?
+
+import java.util.*;
+public class P2 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int first=sc.nextInt();
+        System.out.println("Enter Second Number");
+        int second=sc.nextInt();
+        
+        int add=first + second;
+        System.out.println("Add Two Number : " + add);
+    }
+}
