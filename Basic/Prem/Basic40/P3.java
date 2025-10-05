b277a5a59b6f1e2612c1b0a9b96847cfb5fa185b
prem singh <premshekhawat1997@gmail.com>
2025-10-05 23:17:34 +0530

    3.Print N natural Number

---
+++ Basic/Prem/Basic40/P3.java
@@ -0,0 +1,17 @@
+//3. Print N natural Number ?
+
+import java.util.*;
+public class P3 
+{
+   public static void main(String[] args) 
+   {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter n Number");
+       int n=sc.nextInt();
+       
+       for(int i=1; i<=n; i++)
+       {
+          System.out.println(i);
+       }
+   }   
+}
