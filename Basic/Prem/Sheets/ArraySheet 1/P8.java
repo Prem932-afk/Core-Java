df06e92d72b47797be569eb648d3a5824bd1c969
prem singh <premshekhawat1997@gmail.com>
2025-10-06 19:15:01 +0530

    Program 8

---
+++ Basic/Prem/Sheets/ArraySheet 1/P8.java
@@ -0,0 +1,26 @@
+// Q.8   Find the average of all elements in an array : 
+
+// Explanation: Calculate the average by dividing the sum of elements by the number of elements.
+
+//  Input: arr = [2, 4, 6, 8]
+//  Output: Average: 5.0
+public class P8 
+{
+  public static void main(String[] args) 
+  {
+      int arr[]={2, 4, 6, 8};
+
+      double average=0;
+
+      int total=0;
+
+      for(int i=0; i<arr.length; i++)
+      {
+         total= total + arr[i];
+      }
+
+      average= total / 4;
+
+     System.out.println("Average Number : " + average);
+  }    
+}
