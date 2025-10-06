dd4faa07b27dce85285144b626655d3e24392466
prem singh <premshekhawat1997@gmail.com>
2025-10-06 19:12:40 +0530

    Program 7

---
+++ Basic/Prem/Sheets/ArraySheet 1/P7.java
@@ -0,0 +1,34 @@
+// Q.7  Check if an element exists in the array : 
+// Explanation: Search for a specific number in the array and return "Yes" if found, otherwise "No".
+
+//  Input: arr = [5, 9, 11, 2]
+//  key = 11
+//  Output: yes
+public class P7 
+{
+  public static void main(String[] args) 
+  {
+    int arr[]={5, 9, 11, 2};
+    int key=11;
+
+    boolean found=false;
+    
+    for(int i=0; i<arr.length; i++)
+    {
+          if(arr[i]==key)
+          {
+             found=true;
+             break;
+          }
+    }
+
+    if(found=true)
+    {
+        System.out.println("Yes");
+    }
+    else
+    {
+        System.out.println("Not");
+    }
+  }    
+}
