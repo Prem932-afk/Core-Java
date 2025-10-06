3aeff15900096adc3fd1c5dd78a89d267d5c66b5
prem singh <premshekhawat1997@gmail.com>
2025-10-06 19:09:46 +0530

    Program 6

---
+++ Basic/Prem/Sheets/ArraySheet 1/P6.java
@@ -0,0 +1,28 @@
+// Q.6  Count occurrences of a specific element : 
+// Explanation: Count how many times a specific number (key) appears in the array. Useful for learning conditional statements and counters.
+
+//  Input: arr = [2, 3, 3, 4, 3]
+//  key = 3
+//  Output: Occurrences of 3: 3
+public class P6 
+{
+  public static void main(String[] args) 
+  {
+    int arr[]={2, 3, 3, 4 ,3};
+
+    int key=3;
+    int count =0;
+
+    for(int i=0; i<=arr.length-1; i++)
+    {
+        if(arr[i]==key)
+        {
+           count++;
+        }
+    
+
+    }
+          System.out.println("Occurrences of :  " + key + " : " + count );
+     
+  }    
+}
