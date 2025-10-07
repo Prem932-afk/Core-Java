1d18e084d43839b99338d06a5e0651dabd383d20
prem singh <premshekhawat1997@gmail.com>
2025-10-07 23:17:07 +0530

    P

---
+++ Basic/Prem/Basic40/P6.java
@@ -0,0 +1,19 @@
+//5. Print Even number giving range ?
+
+import java.util.*;
+public class P6
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Number");
+        int n=sc.nextInt();
+
+        for(int i=1; i<=n; i=i+2)
+        {
+              System.out.println(i);
+        }
+        sc.close();
+
+    }
+}
