7f025b30adf5cfb4bcd3f0136a02a1d20b814cc5
prem singh <premshekhawat1997@gmail.com>
2025-10-18 22:57:00 +0530

    Program 5

---
+++ Basic/My/DSA/ArraySheet -1/A5.java
@@ -0,0 +1,41 @@
+// 5. Reverse an array :
+
+// Explanation: Reverse the order of elements in the array. This demonstrates array manipulation and swapping.
+
+// Input: arr = [1, 2, 3, 4]
+// Output: [4, 3, 2, 1]
+
+
+public class A5 
+{
+  public static void main(String[] args) 
+  {
+      int arr[]={1, 2, 3, 4};
+
+    // 1. First Way  
+      
+    //   for(int i=arr.length-1; i>=0; i--)
+    //   {
+    //     System.out.println(arr[i]);
+    //   }
+
+    //2. Second Way
+
+        int i=0;
+        int j=arr.length-1;
+        int temp=0;
+        while(i<j)
+        {
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+
+        for(int k : arr)
+        {
+            System.out.println(k);
+        }
+  }    
+}
