61415e84d92e924bb3726df71d3a397fa22b8643
prem singh <premshekhawat1997@gmail.com>
2025-10-18 23:02:14 +0530

    Program 5

---
+++ Basic/My/Basic 40/B5.java
@@ -0,0 +1,12 @@
+//5.Print odd number?
+public class B5 
+{
+  public static void main(String[] args) 
+  {
+     int n=50;
+     for(int i=1; i<=n; i=i+2)
+     {
+        System.out.println(i);
+     }
+  }    
+}
