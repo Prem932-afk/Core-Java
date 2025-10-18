8ef3976838292811581403257e18792bfbefeb1c
prem singh <premshekhawat1997@gmail.com>
2025-10-18 23:02:47 +0530

    Program 4

---
+++ Basic/My/Basic 40/B4.java
@@ -0,0 +1,15 @@
+//4. Sum of n natural number?
+public class B4 
+{
+  public static void main(String[] args) 
+  {
+      int n=10;
+      int sum=0;
+      for(int i=1; i<=n; i++)
+      {
+        sum=sum + i;
+      } 
+      System.out.println("Sum of N Natural Number :" + sum);
+
+  }    
+}
