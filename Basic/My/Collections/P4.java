a67408c51fa511c6e152903be038c5d8ded3fe63
prem singh <premshekhawat1997@gmail.com>
2025-10-23 14:51:52 +0530

    Program 4

---
+++ Basic/My/Collections/P4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class P4
+{
+    public static void main(String args[])
+    {
+        Collection<String> fruits = new ArrayList();
+        fruits.add("Mango");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        System.out.println(fruits);
+    }
+}
