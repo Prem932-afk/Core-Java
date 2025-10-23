1910de845a82760aa204b521a59c46a0f3e9db3f
prem singh <premshekhawat1997@gmail.com>
2025-10-23 14:54:19 +0530

    Program 5

---
+++ Basic/My/Collections/P5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class P5 
+{
+  public static void main(String[] args) 
+  {
+       List<Integer> number=new ArrayList<>();
+       
+          number.add(12);
+          number.add(2);
+          number.add(20);
+          number.add(1);
+
+          Collections.sort(number);
+
+          System.out.println(number);
+        
+  }    
+}
