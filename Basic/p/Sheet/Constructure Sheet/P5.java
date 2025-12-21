7d48863bcadcfff7e7355b3299c6ffae5581983b
prem singh <premshekhawat1997@gmail.com>
2025-12-21 15:15:44 +0530

    P-5

---
+++ Basic/p/Sheet/Constructure Sheet/P5.java
@@ -0,0 +1,23 @@
+// 5)
+// No-Argument Constructor:
+
+// Define a class Student with a no-argument constructor that initializes name to "Anonymous" and marks to 0.
+
+public class P5 
+{
+  public static void main(String[] args) 
+  {
+     new Student();
+  }    
+}
+
+class Student
+{
+    Student()
+    {
+        String name="Anonymous";
+        int marks=56;
+        System.out.println("Student name :" +name);
+        System.out.println("Student marks :" +marks);
+    }
+}
