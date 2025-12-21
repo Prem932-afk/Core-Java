a3d81ea3af817c9ebb3374e34b86f7d3b4f66f36
prem singh <premshekhawat1997@gmail.com>
2025-12-21 14:41:38 +0530

    P-4

---
+++ Basic/p/Sheet/ArraySheet-1/P4.java
@@ -0,0 +1,27 @@
+
+// 4. Find the smallest element in an array :
+
+// Explanation: Similar to finding the largest element, but track the minimum value instead.
+
+// Input: arr = [8, 6, 3, 1, 9]
+// Output: Smallest: 1
+
+public class P4 
+{
+  public static void main(String[] args) 
+  {
+     int arr[]={8, 6, 4, 1, 9};
+
+     int smallest=arr[0];
+
+     for(int i=0; i<arr.length; i++)
+     {
+          if(arr[i]<smallest)
+          {
+              smallest=arr[i];
+          }
+     }
+
+     System.out.println("Smallest Element : " + smallest);
+  }    
+}
