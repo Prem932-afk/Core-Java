31d82ff5bd949bf0d051377456d28108aebe48ea
prem singh <premshekhawat1997@gmail.com>
2025-12-21 15:14:20 +0530

    P-3

---
+++ Basic/p/Sheet/Constructure Sheet/P3.java
@@ -0,0 +1,25 @@
+//3)
+// Constructor with Parameters:
+
+// Write a class Car that has a constructor to initialize the model and year of a car.
+
+public class P3 
+{
+ public static void main(String[] args) 
+ {
+     car c=new car("swit", 2022);
+ }    
+}
+
+class car
+{
+    String model;
+    int year;
+    car(String model, int year)
+    {
+        this.model=model;
+        this.year=year;
+        System.out.println("Car Model :"+model);
+        System.out.println("Car year :"+year);
+    }
+}
