d4a352627b2b6688482f906c99d04d532f5602dc
prem singh <premshekhawat1997@gmail.com>
2026-01-06 14:45:14 +0530

    Program 4

---
+++ Basic/Helo/Sheet/Constructure Sheet/P4.java
@@ -0,0 +1,27 @@
+
+// 4)
+// Constructor Initialization:
+
+// Create a class Animal with instance variables name and species. Use a constructor to initialize these variables.
+
+public class P4 
+{
+  public static void main(String[] args) 
+  {
+    new Animal("Bird", "Kabutar");
+  }    
+}
+
+class Animal 
+{
+    String name;
+    String species;
+    Animal(String name, String species)
+    {
+        this.name=name;
+        this.species=species;
+
+        System.out.println("Anmial Name : " + name );
+        System.out.println("Animal Species : " + species);
+    }
+}
