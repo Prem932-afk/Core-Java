daeabf70674fc87cee2412191b92148f5de6604c
prem singh <premshekhawat1997@gmail.com>
2026-01-06 14:24:50 +0530

    Porgram 1

---
+++ Basic/Helo/Sheet/ArraySheet-1/P1.java
@@ -0,0 +1,19 @@
+// 1. Print the elements of an array :
+
+//  Explanation: Simply traverse the array and print each element. This teaches students how to iterate through an   array using loops.
+
+//  Input: arr = [1, 2, 3, 4, 5]
+//  Output: 1 2 3 4 5
+
+class P1
+{
+   public static void main(String[] args) 
+   {
+      int arr[]={1, 2, 3, 4, 5};
+      
+      for(int i=0; i<5; i++)
+      {
+        System.out.print(arr[i]);
+      }
+   } 
+}
