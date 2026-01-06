308b64d9163034230e4569a8ef2c1585511e0eb8
prem singh <premshekhawat1997@gmail.com>
2026-01-06 14:39:43 +0530

    Program 2

---
+++ Basic/Helo/Sheet/Constructure Sheet/P2.java
@@ -0,0 +1,25 @@
+//  Q.2)
+ 
+// Default Constructor:
+
+// Write a program to create a class Person with a default constructor that initializes name to "Unknown" and age to 0.
+
+public class P2 
+{
+  public static void main(String[] args) 
+  {
+        Person p=new Person();
+         
+  }   
+}
+
+class Person
+{
+     Person()
+     {
+        String name="Unknown";
+        int age=0;
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+     }
+}
