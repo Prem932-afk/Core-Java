0b691ebd7d0c1e55f7f406eca19358635a9f3df0
prem singh <premshekhawat1997@gmail.com>
2026-03-28 11:50:02 +0530

    8.Enter charectore

---
+++ Basic/N/Basic/B8.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class B8 
+{
+  public static void main(String[] args) 
+  {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter your Charector");
+     char ch=sc.next().charAt(0);
+     
+     System.out.println("Your Charector : " + ch);
+     sc.close();
+  }    
+}
