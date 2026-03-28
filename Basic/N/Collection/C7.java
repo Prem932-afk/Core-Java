d15ea0d0fbc50c503ddf666786f079a0d31c04fd
prem singh <premshekhawat1997@gmail.com>
2026-03-28 14:20:23 +0530

    Program C7

---
+++ Basic/N/Collection/C7.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class C7
+{
+    public static void main(String[] args) 
+    {
+       HashSet h=new HashSet<>();
+       
+       h.add("Rohan");
+       h.add(10);
+       h.add(10);
+       h.add(null);
+       h.add(null);
+       h.add(78);
+       
+
+       System.out.println(h);
+    }
+}
