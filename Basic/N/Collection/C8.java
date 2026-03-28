b9b240ba577cb90994832cc544fadf3f919f90e7
prem singh <premshekhawat1997@gmail.com>
2026-03-28 14:22:56 +0530

    Porgram 8

---
+++ Basic/N/Collection/C8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class C8 
+{
+  public static void main(String[] args) 
+  {
+      TreeSet ts=new TreeSet<>();
+    
+      ts.add(10);
+      ts.add(10);
+      ts.add(20);
+      ts.add(78);
+      ts.add(12);
+      ts.add(1);
+
+      
+      System.out.println(ts);
+  }    
+}
