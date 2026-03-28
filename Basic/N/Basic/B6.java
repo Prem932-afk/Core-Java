22d3f291824e8f635d5aa60a011287221b3095a5
prem singh <premshekhawat1997@gmail.com>
2026-03-28 11:40:23 +0530

    6.Sum Even number

---
+++ Basic/N/Basic/B6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class B6
+{
+  public static void main(String[] args) 
+  {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter your Number");
+
+      int n=sc.nextInt();
+
+      for(int i=1; i<=n; i=i+2)
+      {
+            System.out.println(i);
+      }
+      sc.close();
+  }    
+}
