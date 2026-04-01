e02fdc587db14f0d131872c03466a1ba733d4b29
prem singh <premshekhawat1997@gmail.com>
2026-04-01 14:57:38 +0530

    Program 2

---
+++ Basic/p/P2.java
@@ -0,0 +1,20 @@
+//2. Add Two number
+
+import java.util.Scanner;
+public class P2 
+{
+ public static void main(String[] args) 
+ {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your First Number");
+    int fist=sc.nextInt();
+
+    System.out.println("Enter your Second Number");
+    int second=sc.nextInt();
+
+    int add=fist + second;
+    System.out.println("Sum of Two Number :" +add);
+
+
+ }    
+}
