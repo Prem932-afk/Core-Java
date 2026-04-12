bf0f021cc3f035f1194c0217edadda37508d2931
prem singh <premshekhawat1997@gmail.com>
2026-04-12 10:22:18 +0530

    Vectore

---
+++ Basic/Java Complete/Collection Framework/V1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class V1 
+{
+   public static void main(String[] args) 
+   {
+       Vector v=new Vector<>();
+       v.add("Rahul");
+       v.add(10);
+       v.add(null);
+       v.add(null);
+       v.add(10);
+       
+       System.out.println(v);
+   }    
+}
