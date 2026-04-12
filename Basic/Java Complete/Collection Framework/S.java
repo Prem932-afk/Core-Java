7646f9705a8c99e8ef99e9bb7deb9e712a3f4505
prem singh <premshekhawat1997@gmail.com>
2026-04-12 10:25:41 +0530

    Set

---
+++ Basic/Java Complete/Collection Framework/S.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class S 
+{
+   public static void main(String[] args)
+   {
+      HashSet hs = new HashSet<>();
+
+      hs.add("Rahul");
+      hs.add("Shubam");
+      hs.add(10);
+      hs.add(null);
+      hs.add(null);
+      hs.add(10);
+
+      System.out.println(hs);
+   }    
+}
