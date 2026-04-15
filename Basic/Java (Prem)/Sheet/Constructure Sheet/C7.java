e32217a1ab9e5390a1b6b29c31eafe1ed4736fbf
prem singh <premshekhawat1997@gmail.com>
2026-04-15 11:25:25 +0530

    Program 7

---
+++ Basic/Java (Prem)/Sheet/Constructure Sheet/C7.java
@@ -0,0 +1,26 @@
+
+// 7)
+// Constructor with Multiple Parameters:
+
+// Write a program to create a Rectangle class with length and breadth as instance variables. Use a constructor to initialize them.
+
+
+public class C7 
+{
+  public static void main(String[] args) 
+  {
+     new Rectangle(77, 88);
+  }    
+}
+class Rectangle
+{
+    int length;
+    int breadth;
+    Rectangle(int length, int breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+        System.out.println("Rectangle length : "+ length);
+        System.out.println("Rectangel Breadth :" +breadth);
+    }
+}
