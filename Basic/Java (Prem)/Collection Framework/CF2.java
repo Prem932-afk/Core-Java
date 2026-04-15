fd0c906125d44ea1f3242884f18be8d2a9678297
prem singh <premshekhawat1997@gmail.com>
2026-04-15 12:20:27 +0530

    2. List vs Set

---
+++ Basic/Java (Prem)/Collection Framework/CF2.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+public class CF2 
+{
+  public static void main(String[] args) 
+  {
+     List l = new ArrayList<>();
+
+     l.add(null);
+     l.add(null);
+     l.add(10);
+     l.add(10);
+     l.add("Rahul");
+     l.add("Sharma");
+
+     System.out.println(l);
+
+     System.out.println("...............................................................");
+
+     Set s = new HashSet<>();
+
+     s.add(null);
+     s.add(null);
+     s.add(10);
+     s.add(10);
+     s.add("Rahul");
+     s.add("Sharma");
+
+     System.out.println(s);
+  }
+}
