4719a79abc27ad56de44b39a2ca28eceea9200b7
prem singh <premshekhawat1997@gmail.com>
2026-04-30 09:56:52 +0530

    Program 2

---
+++ Basic/Core Java(C J)/Basic40/P2.java
@@ -0,0 +1,19 @@
+//Q.2 Add two Number ?
+
+import java.util.*;
+public class P2 
+{
+  public static void main(String[] args) 
+  {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter first number");
+     int first=sc.nextInt();
+     
+     System.out.println("Enter second number");
+     int second=sc.nextInt();
+
+     int add= first + second;
+
+     System.out.println("Add Number  : " + add);
+  }    
+}
