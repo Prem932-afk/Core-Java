3374b0f2f35026bc11bd883849eb1edcc6eaad27
prem singh <premshekhawat1997@gmail.com>
2026-05-01 14:51:45 +0530

    Program 3

---
+++ Basic/Core Java(C J)/Collections/V1.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class V1 
+{
+   public static void main(String[] args) 
+   {
+       Vector v=new Vector<>();
+
+       v.add("Rahul");
+       v.add("Rahul");
+       v.add(10);
+       v.add(null);
+       v.add(null);
+
+       System.out.println(v);
+
+       
+   }    
+}
