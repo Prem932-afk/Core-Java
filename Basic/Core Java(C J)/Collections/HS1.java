767283220afc244afcdee575f68e9886b773167c
prem singh <premshekhawat1997@gmail.com>
2026-05-01 14:54:07 +0530

    Program 4

---
+++ Basic/Core Java(C J)/Collections/HS1.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class HS1 
+{
+    public static void main(String[] args) 
+    {
+      HashSet hs=new HashSet<>();
+      
+      hs.add("Rahul");
+      hs.add("Rahul");
+      hs.add(10);
+      hs.add(20);
+      hs.add(null);
+      hs.add(null);
+      hs.add(49);
+
+      System.out.println(hs);
+    }
+}
